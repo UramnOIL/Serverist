2df17c18d337c46a9996cfa520663d8c4981004d
uramnoil <uramnoil@outlook.jp>
2021-03-19 14:30:37 +0900

    :Sparkle: ownerの追加

--- src/main/kotlin/com/uramnoil/serverist/model/server/Server.kt
+++ src/main/kotlin/com/uramnoil/serverist/model/server/Server.kt
@@ -1,4 +1,6 @@
 package com.uramnoil.serverist.model.server
 
-class Server(val id: Id, var name: Name, var address: Address, var port: Port, var description: Description) {
+import com.uramnoil.serverist.model.user.Id as UserId
+
+class Server(val id: Id, var name: Name, val owner: UserId, var address: Address, var port: Port, var description: Description) {
 }
