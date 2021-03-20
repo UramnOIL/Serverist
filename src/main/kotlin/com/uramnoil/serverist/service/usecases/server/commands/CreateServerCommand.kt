a1540886d112e80ec463e0b7d8aa466310d5d780
uramnoil <uramnoil@outlook.jp>
2021-03-21 00:51:39 +0900

    :recycle: リファクタリング

--- src/main/kotlin/com/uramnoil/serverist/service/usecases/server/commands/CreateServerCommand.kt
+++ src/main/kotlin/com/uramnoil/serverist/service/usecases/server/commands/CreateServerCommand.kt
@@ -1,7 +1,7 @@
 package com.uramnoil.serverist.service.usecases.server.commands
 
 interface CreateServerCommand {
-    data class Dto(val name: String, val address: String, val port: Int, val description: String)
+    data class CreateServerDto(val name: String, val address: String, val port: Int, val description: String)
 
-    fun execute(dto: Dto)
+    fun execute(dto: CreateServerDto)
 }
