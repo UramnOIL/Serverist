d74ab7c748aae1815b3a0c34f97a857b4c75a117
uramnoil <uramnoil@outlook.jp>
2021-03-20 23:39:28 +0900

    :recycle: invokeに変更

--- src/main/kotlin/com/uramnoil/serverist/service/usecases/server/CreateServerCommand.kt
+++ src/main/kotlin/com/uramnoil/serverist/service/usecases/server/CreateServerCommand.kt
@@ -3,5 +3,5 @@ package com.uramnoil.serverist.service.usecases.server
 interface CreateServerCommand {
     data class Dto(val name: String, val address: String, val port: Int, val description: String)
 
-    fun command(dto: Dto)
+    fun invoke(dto: Dto)
 }
