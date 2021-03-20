0b528016e8ca5558513ba1a86ec9f4de73ac25d1
uramnoil <uramnoil@outlook.jp>
2021-03-20 19:17:41 +0900

    :sparkle: CreateServerCommandを作成

---
+++ src/main/kotlin/com/uramnoil/serverist/service/usecases/server/CreateServerCommand.kt
@@ -0,0 +1,7 @@
+package com.uramnoil.serverist.service.usecases.server
+
+interface CreateServerCommand {
+    data class Dto(val name: String, val address: String, val port: Int, val description: String)
+
+    fun command(dto: Dto)
+}
