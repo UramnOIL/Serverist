7c06e4124a950655ab934d3967d1ce802b6571f1
uramnoil <uramnoil@outlook.jp>
2021-03-22 23:01:57 +0900

    :heavy_plus_sign: Kodeinを追加

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -4,7 +4,9 @@ plugins {
 
 dependencies {
     val kotlin_version: String by project
+    val kodein_version: String by project
 
     implementation(project(":domain"))
     implementation(kotlin("stdlib", version = kotlin_version))
+    implementation("org.kodein.di", "kodein-di-jvm", kodein_version)
 }
