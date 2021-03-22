d12eae5ed5d2e484d71039eb1885e9e6a0691add
uramnoil <uramnoil@outlook.jp>
2021-03-23 00:20:21 +0900

    :heavy_sign_plus: Kodeinを追加

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -8,5 +8,8 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
 
 dependencies {
     val kotlin_version: String by project
+    val kodein_version: String by project
+
     implementation(kotlin("stdlib", version = kotlin_version))
+    implementation("org.kodein.di", "kodein-di-jvm", kodein_version)
 }
