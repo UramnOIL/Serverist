010154b01c36e9a84a94ab498116852afc8c553a
uramnoil <uramnoil@outlook.jp>
2021-03-22 20:44:15 +0900

    :heavy_plus_sign: Kodeinを追加

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -7,16 +7,22 @@ application {
     mainClassName = "io.ktor.server.netty.EngineMain"
 }
 
+repositories {
+    mavenCentral()
+}
+
 dependencies {
     val kotlin_version: String by project
     val ktor_version: String by project
     val logback_version: String by project
     val kgraphql_version: String by project
+    val kodein_version: String by project
 
     implementation(project(":application"))
     implementation(kotlin("stdlib", kotlin_version))
     implementation("io.ktor", "ktor-server-netty", ktor_version)
     implementation("ch.qos.logback", "logback-classic", logback_version)
-    implementation("com.apurebase", "kgraphql:", kgraphql_version)
+    implementation("com.apurebase", "kgraphql", kgraphql_version)
+    implementation("org.kodein.di", "kodein-di-jvm", kodein_version)
     testImplementation("io.ktor", "ktor-server-tests", ktor_version)
 }
