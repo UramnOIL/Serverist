6b13ba9716d1ad254eb7852b980dd71c35b6dd25
uramnoil <uramnoil@outlook.jp>
2021-03-23 01:10:16 +0900

    :sparkles: コンストラクタのアクセス修飾子をinternalに変更

--- domain/src/main/kotlin/com.uramnoil.serverist.domain/service/models/server/Server.kt
+++ domain/src/main/kotlin/com.uramnoil.serverist.domain/service/models/server/Server.kt
@@ -3,5 +3,12 @@ package com.uramnoil.serverist.service.models.server
 import com.uramnoil.serverist.domain.service.models.server.*
 import com.uramnoil.serverist.domain.service.models.user.Id as UserId
 
-class Server(val id: Id, var name: Name, val owner: UserId, var address: Address, var port: Port, var description: Description) {
+class Server internal constructor(
+    val id: Id,
+    var name: Name,
+    val owner: UserId,
+    var address: Address,
+    var port: Port,
+    var description: Description
+) {
 }
--- domain/src/main/kotlin/com.uramnoil.serverist.domain/service/models/user/User.kt
+++ domain/src/main/kotlin/com.uramnoil.serverist.domain/service/models/user/User.kt
@@ -1,8 +1,4 @@
 package com.uramnoil.serverist.domain.service.models.user
 
-import com.uramnoil.serverist.domain.service.models.user.Description
-import com.uramnoil.serverist.domain.service.models.user.Id
-import com.uramnoil.serverist.domain.service.models.user.Name
-
-class User(val id: Id, val name: Name, val description: Description) {
+class User internal constructor(val id: Id, val name: Name, val description: Description) {
 }
