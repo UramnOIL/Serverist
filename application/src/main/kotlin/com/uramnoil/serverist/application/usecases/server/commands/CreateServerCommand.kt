f47ca47623af7d101a702712852d13b4e8b5a16c
uramnoil <uramnoil@outlook.jp>
2021-03-22 16:02:47 +0900

    :art: address, port, descriptionの型をNullableに変更

--- application/src/main/kotlin/com/uramnoil/serverist/application/usecases/server/commands/CreateServerCommand.kt
+++ application/src/main/kotlin/com/uramnoil/serverist/application/usecases/server/commands/CreateServerCommand.kt
@@ -1,7 +1,7 @@
 package com.uramnoil.serverist.application.usecases.server.commands
 
 interface CreateServerCommand {
-    data class CreateServerDto(val name: String, val address: String, val port: Int, val description: String)
+    data class CreateServerDto(val ownerId: String, val name: String, val address: String?, val port: Int?, val description: String?)
 
     fun execute(dto: CreateServerDto)
 }
