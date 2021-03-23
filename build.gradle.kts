dcefef3c09ba7f6d8d9aeb5aabbf250425b8414f
uramnoil <uramnoil@outlook.jp>
2021-03-23 19:54:46 +0900

    :heavy_plus_sign: GradleVersionPluginを追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,16 @@
 group = "com.uramnoil"
 version = "0.0.1"
 
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.github.ben-manes.versions") version "0.38.0"
+}
+
 allprojects {
     repositories {
         mavenLocal()
