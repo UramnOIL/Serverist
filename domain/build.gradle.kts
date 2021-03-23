ae160f907850541f370f6655d7fa294baa8ccb0d
uramnoil <uramnoil@outlook.jp>
2021-03-23 16:19:08 +0900

    :heavy_plus_sign: SQLDelightを追加

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm")
+    id("com.squareup.sqldelight")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
@@ -10,8 +11,10 @@ dependencies {
     val kotlin_version: String by project
     val kodein_version: String by project
     val coroutines_version: String by project
+    val sqldelight_version: String by project
 
     implementation(kotlin("stdlib", version = kotlin_version))
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", coroutines_version)
     implementation("org.kodein.di", "kodein-di-jvm", kodein_version)
+    implementation("com.squareup.sqldelight", "sqlight-driver", sqldelight_version)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,8 @@ rootProject.name = "serverist"
 pluginManagement {
     plugins {
         kotlin("jvm") version "1.4.31"
+        id("com.squareup.sqldelight") version "1.4.4"
     }
 }
 
-include(":domain", ":application", ":web")
+include(":domain", ":application", ":backend")
