bee01e832f31bbbc00dfc2a44c91850cc31d326f
uramnoil <uramnoil@outlook.jp>
2021-03-24 16:46:19 +0900

    :art: 返り値の型をIdに変更

--- domain/src/main/kotlin/com/uramnoil/serverist/domain/service/services/user/CreateUserService.kt
+++ domain/src/main/kotlin/com/uramnoil/serverist/domain/service/services/user/CreateUserService.kt
@@ -1,8 +1,8 @@
 package com.uramnoil.serverist.domain.service.services.user
 
-import com.uramnoil.serverist.domain.service.models.user.User
+import com.uramnoil.serverist.domain.service.models.user.Id
 import kotlinx.coroutines.Deferred
 
 interface CreateUserService {
-    fun newAsync(name: String, description: String): Deferred<User>
+    fun newAsync(name: String, description: String): Deferred<Id>
 }
