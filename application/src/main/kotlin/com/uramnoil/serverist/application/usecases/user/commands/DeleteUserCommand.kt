62734b76d8c4bcd734e7becd11be33e774a9aabc
uramnoil <uramnoil@outlook.jp>
2021-03-24 19:17:10 +0900

    :bug: 引数の型を修正

--- application/src/main/kotlin/com/uramnoil/serverist/application/usecases/user/commands/DeleteUserCommand.kt
+++ application/src/main/kotlin/com/uramnoil/serverist/application/usecases/user/commands/DeleteUserCommand.kt
@@ -3,7 +3,7 @@ package com.uramnoil.serverist.application.usecases.user.commands
 import java.util.*
 
 fun interface DeleteUserCommand {
-    fun execute(dto: DeleteUserCommand)
+    fun execute(dto: DeleteUserCommandDto)
 }
 
 data class DeleteUserCommandDto(val id: UUID)
