a7f627e78855b8a2ad34e80fcc8369e65cc3ad7e
uramnoil <uramnoil@outlook.jp>
2021-03-24 23:23:30 +0900

    :heavy_plus_sign: H2

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation("org.jetbrains.exposed", "exposed-core", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-dao", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-jdbc", exposedVersion)
+    implementation("com.h2database", "h2", "1.4.200")
 
     testImplementation("io.ktor", "ktor-server-tests", ktorVersion)
 }
