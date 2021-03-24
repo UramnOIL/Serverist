28188935065189967bb67b943b5505981c894e3e
uramnoil <uramnoil@outlook.jp>
2021-03-24 18:55:36 +0900

    :sparkles: ExposedCreateUserServiceを追加

---
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/ExposedCreateUserService.kt
@@ -0,0 +1,23 @@
+package com.uramnoil.serverist.infrastracture
+
+import com.uramnoil.serverist.domain.service.models.user.Id
+import com.uramnoil.serverist.domain.service.services.user.CreateUserService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.coroutines.CoroutineContext
+
+class ExposedCreateUserService(private val database: Database, private val context: CoroutineContext) :
+    CreateUserService, CoroutineScope by CoroutineScope(context) {
+    override fun newAsync(name: String, description: String): Deferred<Id> = async {
+        transaction(database) {
+            Id(Users.insertAndGetId {
+                it[Users.name] = name
+                it[Users.description] = description
+            }.value)
+        }
+    }
+}
