242b0bfe3cfc592c379192a0656fe2389b5ecce2
uramnoil <uramnoil@outlook.jp>
2021-03-25 00:50:08 +0900

    :art: テーブル名を設定

--- backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/Servers.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/Servers.kt
@@ -2,10 +2,10 @@ package com.uramnoil.serverist.infrastracture
 
 import org.jetbrains.exposed.dao.id.UUIDTable
 
-object Servers : UUIDTable() {
+object Servers : UUIDTable("Servers") {
     val name = text("name")
     val owner = uuid("owner").references(Users.id)
     val address = text("address").nullable()
     val port = integer("port").nullable()
-    val description = text("name")
+    val description = text("description")
 }
--- backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/Users.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/Users.kt
@@ -2,7 +2,7 @@ package com.uramnoil.serverist.infrastracture
 
 import org.jetbrains.exposed.dao.id.UUIDTable
 
-object Users : UUIDTable() {
+object Users : UUIDTable("Users") {
     val name = text("name")
     val description = text("description")
 }
