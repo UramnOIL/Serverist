eabae687ea75ac28da3b90ae945882f28e0ccf05
uramnoil <uramnoil@outlook.jp>
2021-03-25 01:36:54 +0900

    :white_check_mark: サーバー追加テスト

--- backend/src/test/kotlin/H2Test.kt
+++ backend/src/test/kotlin/H2Test.kt
@@ -1,5 +1,8 @@
+import com.uramnoil.serverist.application.usecases.server.commands.CreateServerCommand
+import com.uramnoil.serverist.application.usecases.server.commands.CreateServerDto
 import com.uramnoil.serverist.application.usecases.user.commands.CreateUserCommand
 import com.uramnoil.serverist.application.usecases.user.commands.CreateUserDto
+import com.uramnoil.serverist.infrastracture.Servers
 import com.uramnoil.serverist.infrastracture.Users
 import com.uramnoil.serverist.infrastracture.buildDi
 import kotlinx.coroutines.CoroutineScope
@@ -10,22 +13,13 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
-import org.jetbrains.exposed.sql.transactions.transaction
 import org.kodein.di.instance
-import kotlin.test.BeforeTest
 import kotlin.test.Test
 
 class H2Test {
     val database = Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver")
     val di = buildDi(database, Dispatchers.Default)
     val scope = CoroutineScope(Dispatchers.Default)
-
-    @BeforeTest
-    fun `テーブル初期化`() {
-        transaction {
-            commit()
-        }
-    }
 
     @Test
     fun `ユーザーを作成`() {
@@ -34,12 +28,52 @@ class H2Test {
         runBlocking {
             newSuspendedTransaction(db = database) {
                 SchemaUtils.create(Users)
+                delay(50)
                 command.execute(CreateUserDto("UramnOIL", "うんちぶり"))
+                delay(50)
+                Users.selectAll().forEach {
+                    println("ID: ${it[Users.id]}, Name: ${it[Users.name]}, Description: ${it[Users.description]}")
+                }
+            }
+        }
+    }
 
-                delay(500)
+    @Test
+    fun `サーバーを作成`() {
+        val createUserCommand: CreateUserCommand by di.instance()
+        val createServerCommand: CreateServerCommand by di.instance()
+
+        runBlocking {
+            newSuspendedTransaction(db = database) {
+                SchemaUtils.create(Users)
+                SchemaUtils.create(Servers)
+
+                createUserCommand.execute(CreateUserDto("UramnOIL", "うんちぶり"))
+                commit()
+
+                delay(10)
+
+                val user = Users.selectAll().first()
 
                 Users.selectAll().forEach {
-                    println("id: ${it[Users.id]}, name: ${it[Users.name]}, description: ${it[Users.description]}")
+                    println("ID: ${it[Users.id]}, Name: ${it[Users.name]}, Description: ${it[Users.description]}")
+                }
+
+                createServerCommand.execute(
+                    CreateServerDto(
+                        user[Users.id].value,
+                        "HogeServer",
+                        "uramnoil.com",
+                        19132,
+                        "うんちぶりぶり"
+                    )
+                )
+                commit()
+
+                delay(10)
+
+                Servers.selectAll().forEach {
+                    println("ID: ${it[Servers.id]}, OwnerID: ${it[Servers.owner]}, Name: ${it[Servers.name]}, Address: ${it[Servers.address]}, Port: ${it[Servers.port]}, Description: ${it[Servers.description]}")
                 }
             }
         }
