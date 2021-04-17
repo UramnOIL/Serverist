6698c6a023949408013aaa7dfde91009be68dcd3
uramnoil <uramnoil@outlook.jp>
2021-04-18 03:20:42 +0900

    :sparkles: UserIdを作成

--- domain/src/main/kotlin/com/uramnoil/serverist/domain/login/models/user/User.kt
+++ domain/src/main/kotlin/com/uramnoil/serverist/domain/login/models/user/User.kt
@@ -1,4 +1,4 @@
 package com.uramnoil.serverist.domain.login.models.user
 
-class User(val entityId: EntityId, val password: HashPassword) {
+class User(val entityId: EntityId, val userId: UserId, val password: HashPassword) {
 }
