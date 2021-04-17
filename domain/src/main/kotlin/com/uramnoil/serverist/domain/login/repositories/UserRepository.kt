dd7145c852e66002c30dfdb31e1969763a464f02
uramnoil <uramnoil@outlook.jp>
2021-04-18 02:32:12 +0900

    :heavy_plus_sign: UserRepositoryを作成

---
+++ domain/src/main/kotlin/com/uramnoil/serverist/domain/login/repositories/UserRepository.kt
@@ -0,0 +1,11 @@
+package com.uramnoil.serverist.domain.login.repositories
+
+import com.uramnoil.serverist.domain.login.models.user.Id
+import com.uramnoil.serverist.domain.login.models.user.User
+import kotlinx.coroutines.Deferred
+
+interface UserRepository {
+    fun storeAsync(user: User): Deferred<Unit>
+    fun deleteAsync(): Deferred<Unit>
+    fun findByIdAsync(id: Id): Deferred<Unit>
+}
