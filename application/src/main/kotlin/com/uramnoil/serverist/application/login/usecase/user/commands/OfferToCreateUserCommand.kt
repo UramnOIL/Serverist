a726b9b6947a5c3dc8073c258e14fcce6b07f88b
uramnoil <uramnoil@outlook.jp>
2021-04-18 18:12:47 +0900

    :sparkles: OfferToCreateUserCommandを作成

---
+++ application/src/main/kotlin/com/uramnoil/serverist/application/login/usecase/user/commands/OfferToCreateUserCommand.kt
@@ -0,0 +1,21 @@
+package com.uramnoil.serverist.application.login.usecase.user.commands
+
+data class OfferToCreateUserCommandDto(val id: String, val email: String, val password: String, val name: String)
+
+fun interface OfferToCreateUserCommand {
+    fun execute(dto: OfferToCreateUserCommandDto)
+}
+
+data class OfferToCreateUserCommandOutputPortDto(val jwt: String)
+
+enum class OfferToCreateUserCommandError {
+    AlreadyCreated,
+}
+
+data class OfferToCreateUserCommandOutputPortErrorDto(val error: OfferToCreateUserCommandError)
+
+interface OfferToCreateUserCommandOutputPort {
+    fun handle(dto: OfferToCreateUserCommandOutputPortDto)
+
+    fun error(dto: OfferToCreateUserCommandOutputPortErrorDto)
+}
