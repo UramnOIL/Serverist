bee9b307a6a93403b3df96aa83068ec0bfecc5a5
uramnoil <uramnoil@outlook.jp>
2021-04-24 17:59:41 +0900

    :sparkles: findByIdAsyncを追加

--- domain/src/main/kotlin/com/uramnoil/serverist/domain/service/repositories/UserRepository.kt
+++ domain/src/main/kotlin/com/uramnoil/serverist/domain/service/repositories/UserRepository.kt
@@ -1,9 +1,11 @@
 package com.uramnoil.serverist.domain.service.repositories
 
-import com.uramnoil.serverist.domain.login.models.user.User
+import com.uramnoil.serverist.domain.service.models.user.Id
+import com.uramnoil.serverist.domain.service.models.user.User
 import kotlinx.coroutines.Deferred
 
 interface UserRepository {
     fun storeAsync(user: User): Deferred<Unit>
     fun deleteAsync(user: User): Deferred<Unit>
+    fun findByIdAsync(id: Id): Deferred<User>
 }
