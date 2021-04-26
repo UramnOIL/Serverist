a2216318e322c7fa0e7f81f3061dfaf6ef3022bf
uramnoil <uramnoil@outlook.jp>
2021-04-26 16:23:26 +0900

    :sparkles: findByAccountIdを作成

--- domain/src/main/kotlin/com/uramnoil/serverist/domain/repositories/UnapprovedUserRepository.kt
+++ domain/src/main/kotlin/com/uramnoil/serverist/domain/repositories/UnapprovedUserRepository.kt
@@ -1,9 +1,11 @@
 package com.uramnoil.serverist.domain.repositories
 
+import com.uramnoil.serverist.domain.models.unapproveduser.AccountId
 import com.uramnoil.serverist.domain.models.unapproveduser.User
 import kotlinx.coroutines.Deferred
 
 interface UnapprovedUserRepository {
     fun storeAsync(user: User): Deferred<Unit>
     fun deleteAsync(remove: User): Deferred<Unit>
+    fun findByAccountId(id: AccountId): Deferred<User>
 }
