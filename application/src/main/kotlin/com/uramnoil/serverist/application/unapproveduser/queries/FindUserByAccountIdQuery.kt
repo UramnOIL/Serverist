4cea4ada1c9b8b88864672aaac5e5a3361abc99f
uramnoil <uramnoil@outlook.jp>
2021-04-26 17:33:20 +0900

    :art: Functional Interfaceに変更

--- application/src/main/kotlin/com/uramnoil/serverist/application/unapproveduser/queries/FindUserByAccountIdQuery.kt
+++ application/src/main/kotlin/com/uramnoil/serverist/application/unapproveduser/queries/FindUserByAccountIdQuery.kt
@@ -4,12 +4,12 @@ import com.uramnoil.serverist.application.unapproveduser.User
 
 data class FindUserByAccountIdQueryDto(val id: String)
 
-interface FindUserByAccountIdQuery {
+fun interface FindUserByAccountIdQuery {
     fun execute(dto: FindUserByAccountIdQueryDto)
 }
 
 data class FindUserByAccountIdQueryOutputPortDto(val user: User?)
 
-interface FindUserByAccountIdQueryOutputPort {
+fun interface FindUserByAccountIdQueryOutputPort {
     fun handle(dto: FindUserByAccountIdQueryOutputPort)
 }
