b43aaa08664d18f193861235b0f43035533d360e
uramnoil <uramnoil@outlook.jp>
2021-05-05 05:09:57 +0900

    :art: VOを追加

--- backend/src/main/kotlin/com/uramnoil/serverist/domain/models/user/User.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/domain/models/user/User.kt
@@ -2,7 +2,9 @@ package com.uramnoil.serverist.domain.models.user
 
 class User(
     val id: Id,
+    val email: Email,
     var accountId: AccountId,
+    var hashedPassword: HashedPassword,
     var name: Name,
     var description: Description
 )
