b5ddeff3e8db54e2da9949c4bce14bab8c599d84
uramnoil <uramnoil@outlook.jp>
2021-05-12 22:37:56 +0900

    :rotating_light: JVMターゲットを追加

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -7,8 +7,13 @@ application {
     mainClassName = "io.ktor.server.netty.EngineMain"
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions.jvmTarget = "1.8"
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
 }
 
 repositories {
