f255f8bd19257d4b49e9d0efe77b406ca347aab0
uramnoil <uramnoil@outlook.jp>
2021-05-15 10:40:04 +0900

    :sparkles: createを作成

--- backend/src/main/kotlin/com/uramnoil/serverist/domain/services/server/ServerFactory.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/domain/services/server/ServerFactory.kt
@@ -6,10 +6,20 @@ import com.uramnoil.serverist.domain.models.user.Id as UserId
 
 object ServerFactory {
     fun create(id: String, name: String, ownerId: String, address: String?, port: Int?, description: String) =
+        create(
+            UUID.fromString(id),
+            name,
+            UUID.fromString(ownerId),
+            address,
+            port,
+            description
+        )
+
+    fun create(id: UUID, name: String, ownerId: UUID, address: String?, port: Int?, description: String) =
         Server(
-            Id(UUID.fromString(id)),
+            Id(id),
             Name(name),
-            UserId(UUID.fromString(ownerId)),
+            UserId(ownerId),
             Address(address),
             Port(port),
             Description(description)
