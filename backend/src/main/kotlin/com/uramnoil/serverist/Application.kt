fdc9531f8de8d157b838abe46604269252019e3e
uramnoil <uramnoil@outlook.jp>
2021-05-15 09:11:22 +0900

    :art: とりあえず

--- backend/src/main/kotlin/com/uramnoil/serverist/Application.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/Application.kt
@@ -1,23 +1,23 @@
 package com.uramnoil.serverist
 
 import com.apurebase.kgraphql.GraphQL
-import com.uramnoil.serverist.application.unauthenticateduser.queries.FindUnauthenticatedUserByAccountIdQuery
-import com.uramnoil.serverist.application.unauthenticateduser.queries.FindUnauthenticatedUserByAccountIdQueryOutputPort
-import com.uramnoil.serverist.application.user.queries.FindUserByNameDto
-import com.uramnoil.serverist.application.user.queries.FindUserByNameQuery
-import com.uramnoil.serverist.infrastracture.server.Servers
-import com.uramnoil.serverist.infrastracture.user.Users
+import com.uramnoil.serverist.application.unauthenticateduser.service.RequestToCreateUserService
+import com.uramnoil.serverist.application.unauthenticateduser.service.RequestToCreateUserServiceDto
+import com.uramnoil.serverist.application.user.User
+import com.uramnoil.serverist.application.user.queries.ValidateLoginService
+import com.uramnoil.serverist.application.user.queries.ValidateLoginServiceDto
 import io.ktor.application.*
 import io.ktor.auth.*
+import io.ktor.http.*
 import io.ktor.request.*
+import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.sessions.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.transaction
 import org.kodein.di.instance
+import java.util.*
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -37,17 +37,7 @@ fun Application.module(testing: Boolean = false) {
 
     val di = buildApplicationDi(buildDomainDi(database, context), database, context)
 
-    if (testing) {
-        transaction(database) {
-            SchemaUtils.drop(Users, Servers)
-        }
-    }
-
-    transaction {
-        SchemaUtils.create(Users, Servers)
-    }
-
-    data class AuthSession(val id: String)
+    data class AuthSession(val id: UUID) : Principal
 
     install(Sessions) {
         cookie<AuthSession>("SESSION", if (testing) TODO("Redis用Storageを作成") else SessionStorageMemory()) {
@@ -64,44 +54,54 @@ fun Application.module(testing: Boolean = false) {
         }
     }
 
-
-
-    install(GraphQL) {
-        playground = true
-
-        schema {
-            query("findUserByAccountId") {
-                resolver { ->
-                    val query: FindUnauthenticatedUserByAccountIdQuery by di.instance()
-                }
-            }
-        }
-    }
-
     data class UserPasswordPrincipal(val user: String, val password: String) : Principal
 
     routing {
         post("login") {
             data class IdEmailPassword(val idOrEmail: String, val password: String)
             call.receive<IdEmailPassword>().let { idEmailPassword ->
-                val output = FindUnauthenticatedUserByAccountIdQueryOutputPort { dto ->
-                    dto.let {
-
-                    } ?: let {
-                        TODO("Emailで検索")
-                    }
+                call.sessions.get<AuthSession>()?.let {
+                    return@post
                 }
 
-                val query: FindUserByNameQuery by di.instance(arg = output)
+                val service: ValidateLoginService by di.instance()
 
-                query.execute(FindUserByNameDto(idEmailPassword.idOrEmail))
+                val user: User =
+                    service.execute(ValidateLoginServiceDto(idEmailPassword.idOrEmail, idEmailPassword.password))
+                        ?: return@post call.respond(HttpStatusCode.Unauthorized)
+
+                call.sessions.set(AuthSession(user.id))
+
+                call.respond(HttpStatusCode.OK)
             }
         }
 
         post("signup") {
-            data class IdEmailPassword(val id: String, val email: String, val password: String)
+            data class IdEmailPassword(val accountId: String, val email: String, val password: String)
             call.receive<IdEmailPassword>().let {
+                val service: RequestToCreateUserService by di.instance()
+                service.execute(RequestToCreateUserServiceDto(it.accountId, it.email, it.password))
+            }
+        }
+    }
 
+    install(GraphQL) {
+        playground = true
+
+        wrap {
+            authenticate(optional = true, build = it)
+        }
+
+        context { call ->
+            call.authentication.principal<AuthSession>()?.let {
+                +it
+            }
+        }
+
+        schema {
+            stringScalar<UUID> {
+                deserialize = { UUID.fromString(it) }
+                serialize = UUID::toString
             }
         }
     }
