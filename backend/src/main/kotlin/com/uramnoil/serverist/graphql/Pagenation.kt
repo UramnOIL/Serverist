75394751e773c54656d577a4a48b510491a002ba
uramnoil <uramnoil@outlook.jp>
2021-05-15 10:45:15 +0900

    :art: Longに変更

--- backend/src/main/kotlin/com/uramnoil/serverist/graphql/Pagenation.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/graphql/Pagenation.kt
@@ -1,5 +1,5 @@
 package com.uramnoil.serverist.graphql
 
-data class PageRequest(val limit: Int, val offset: Int)
+data class PageRequest(val limit: Int, val offset: Long)
 
-data class PageInfo(val limit: Int, val offset: Int)
+data class PageInfo(val limit: Int, val offset: Long)
