ff613480e859c3698afa0058d5979f10c1f5f985
uramnoil <uramnoil@outlook.jp>
2021-05-15 13:05:07 +0900

    :bug: datetime

--- backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/server/ExposedServerRepository.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/server/ExposedServerRepository.kt
@@ -3,31 +3,20 @@ package com.uramnoil.serverist.infrastracture.server
 import com.uramnoil.serverist.domain.models.server.Id
 import com.uramnoil.serverist.domain.models.server.Server
 import com.uramnoil.serverist.domain.repositories.ServerRepository
-import com.uramnoil.serverist.domain.services.server.ServerFactory
-import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.update
 
-class ExposedServerRepository(private val database: Database) : ServerRepository {
-    override suspend fun findById(id: Id): Server? = newSuspendedTransaction(db = database) {
+class ExposedServerRepository : ServerRepository {
+    override suspend fun findById(id: Id): Server? = newSuspendedTransaction {
         val result = Servers.select { Servers.id eq id.value }.firstOrNull() ?: return@newSuspendedTransaction null
 
-        result.let {
-            ServerFactory.create(
-                it[Servers.id].value.toString(),
-                it[Servers.name],
-                it[Servers.owner].toString(),
-                it[Servers.address],
-                it[Servers.port],
-                it[Servers.description]
-            )
-        }
+        ExposedServerFactory.create(result)
     }
 
-    override suspend fun store(server: Server) = newSuspendedTransaction(db = database) {
+    override suspend fun store(server: Server) = newSuspendedTransaction {
         Servers.update({ Servers.id eq server.id.value }) {
             it[name] = server.name.value
             it[address] = server.address.value
