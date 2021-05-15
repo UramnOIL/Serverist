6cf1f082534fca0befbeb09bd99af777018a250d
uramnoil <uramnoil@outlook.jp>
2021-05-15 10:00:32 +0900

    :art: オフセット法に変更

--- backend/src/main/kotlin/com/uramnoil/serverist/graphql/Pagenation.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/graphql/Pagenation.kt
@@ -1,10 +1,5 @@
 package com.uramnoil.serverist.graphql
 
-data class PageRequest(val first: Int?, val after: String?, val last: Int?, val before: Int?)
+data class PageRequest(val limit: Int, val offset: Int)
 
-data class PageInfo(
-    val endCursor: String,
-    val hasNextPage: Boolean,
-    val hasPreviousPage: Boolean,
-    val startCursor: String
-)
+data class PageInfo(val limit: Int, val offset: Int)
