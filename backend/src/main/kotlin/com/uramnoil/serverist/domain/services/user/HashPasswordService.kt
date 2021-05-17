003542972af03e99ff0d165b0ccf6bb4569b5348
uramnoil <uramnoil@outlook.jp>
2021-05-17 23:04:41 +0900

    :art: 戻り値を追加

--- backend/src/main/kotlin/com/uramnoil/serverist/domain/services/user/HashPasswordService.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/domain/services/user/HashPasswordService.kt
@@ -2,5 +2,5 @@ package com.uramnoil.serverist.domain.services.user
 
 interface HashPasswordService {
     fun hash(password: String): String
-    fun check(password: String, hash: String)
+    fun check(password: String, hash: String): Boolean
 }
