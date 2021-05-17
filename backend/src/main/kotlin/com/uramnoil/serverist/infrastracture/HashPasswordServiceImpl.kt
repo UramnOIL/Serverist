7a911079dadccd99dac5511d0f1e4798dc5edd46
uramnoil <uramnoil@outlook.jp>
2021-05-18 00:56:27 +0900

    :art: privateに変更

--- backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/HashPasswordServiceImpl.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/HashPasswordServiceImpl.kt
@@ -4,7 +4,7 @@ import com.uramnoil.serverist.domain.services.user.HashPasswordService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 class HashPasswordServiceImpl : HashPasswordService {
-    val encoder = BCryptPasswordEncoder()
+    private val encoder = BCryptPasswordEncoder()
 
     override fun hash(row: String): String = encoder.encode(row)
 
