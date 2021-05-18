309dac385aa12cace25dc7d9378ae1108233adea
uramnoil <uramnoil@outlook.jp>
2021-05-19 01:36:35 +0900

    :art: ファイルに分割

--- backend/src/main/kotlin/com/uramnoil/serverist/application/server/queries/FindAllServersQuery.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/application/server/queries/FindAllServersQuery.kt
@@ -1,15 +1,7 @@
 package com.uramnoil.serverist.application.server.queries
 
+import com.uramnoil.serverist.application.Sort
 import com.uramnoil.serverist.application.server.Server
-
-enum class Sort {
-    Desc,
-    Asc,
-}
-
-enum class OrderBy {
-    CreatedAt,
-}
 
 data class FindAllServerQueryDto(val limit: Int, val offset: Long, val sort: Sort, val orderBy: OrderBy)
 
