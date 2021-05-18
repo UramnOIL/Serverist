b754bf3f4f29e779000d0f79249bbfb7590b38dc
uramnoil <uramnoil@outlook.jp>
2021-05-19 02:07:26 +0900

    :art: リネーム

--- backend/src/main/kotlin/com/uramnoil/serverist/application/server/queries/FindServersByOwnerQuery.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/application/server/queries/FindServersByOwnerQuery.kt
@@ -3,8 +3,8 @@ package com.uramnoil.serverist.application.server.queries
 import com.uramnoil.serverist.application.Sort
 import com.uramnoil.serverist.application.server.Server
 
-data class FindServersByOwnerDto(val limit: Int, val offset: Long, val sort: Sort, val orderBy: OrderBy)
+data class FindServersByOwnerQueryDto(val limit: Int, val offset: Long, val sort: Sort, val orderBy: OrderBy)
 
 interface FindServersByOwnerQuery {
-    suspend fun execute(dto: FindAllServerQueryDto): List<Server>
+    suspend fun execute(dto: FindServersByOwnerQueryDto): List<Server>
 }
