ad709ef70448e8425de9d0757c974e1d42cd8a23
uramnoil <uramnoil@outlook.jp>
2021-05-19 02:08:29 +0900

    :art: ownerIdを追加

--- backend/src/main/kotlin/com/uramnoil/serverist/application/server/queries/FindServersByOwnerQuery.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/application/server/queries/FindServersByOwnerQuery.kt
@@ -2,8 +2,15 @@ package com.uramnoil.serverist.application.server.queries
 
 import com.uramnoil.serverist.application.Sort
 import com.uramnoil.serverist.application.server.Server
+import java.util.*
 
-data class FindServersByOwnerQueryDto(val limit: Int, val offset: Long, val sort: Sort, val orderBy: OrderBy)
+data class FindServersByOwnerQueryDto(
+    val ownerId: UUID,
+    val limit: Int,
+    val offset: Long,
+    val sort: Sort,
+    val orderBy: OrderBy
+)
 
 interface FindServersByOwnerQuery {
     suspend fun execute(dto: FindServersByOwnerQueryDto): List<Server>
