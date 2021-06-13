f5efb818b4dcff77cdd43c42ab6322dd2181f84a
uramnoil <uramnoil@outlook.jp>
2021-06-13 17:57:54 +0900

    :art: 移動

--- backend/src/main/kotlin/com/uramnoil/serverist/graphql/Pagenation.kt
+++
@@ -1,3 +0,0 @@
-package com.uramnoil.serverist.graphql
-
-data class PageRequest(val limit: Int, val offset: Long)
--- backend/src/main/kotlin/com/uramnoil/serverist/graphql/Utils.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/graphql/Utils.kt
@@ -3,4 +3,7 @@ package com.uramnoil.serverist.graphql
 import com.apurebase.kgraphql.Context
 import com.uramnoil.serverist.AuthSession
 
+
+data class PageRequest(val limit: Int, val offset: Long)
+
 fun checkSession(ctx: Context): Exception? = if (ctx.get<AuthSession>() == null) Exception("権限がありません。") else null
