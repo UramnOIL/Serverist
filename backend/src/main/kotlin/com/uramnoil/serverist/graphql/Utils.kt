e387ad6d88a648254332a50f0daca8eeb6f57bf3
uramnoil <uramnoil@outlook.jp>
2021-06-13 18:13:22 +0900

    :sparkles: getIdFromSessionを作成

--- backend/src/main/kotlin/com/uramnoil/serverist/graphql/Utils.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/graphql/Utils.kt
@@ -2,8 +2,11 @@ package com.uramnoil.serverist.graphql
 
 import com.apurebase.kgraphql.Context
 import com.uramnoil.serverist.AuthSession
+import java.util.*
 
 
 data class PageRequest(val limit: Int, val offset: Long)
 
 fun checkSession(ctx: Context): Exception? = if (ctx.get<AuthSession>() == null) Exception("権限がありません。") else null
+
+fun getIdFromSession(ctx: Context): UUID = ctx.get<AuthSession>()!!.id
