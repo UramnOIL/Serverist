f092886caf68d5c9f582adb5be69d4112da5d1e7
uramnoil <uramnoil@outlook.jp>
2021-06-15 22:30:38 +0900

    :sparkles: ExposedFindUserByIdQueryを作成

---
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/user/ExposedFindUserByIdQuery.kt
@@ -0,0 +1,14 @@
+package com.uramnoil.serverist.infrastracture.user
+
+import com.uramnoil.serverist.application.kernel.User
+import com.uramnoil.serverist.application.user.queries.FindUserByIdQuery
+import com.uramnoil.serverist.application.user.queries.FindUserByIdQueryDto
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+
+class ExposedFindUserByIdQuery : FindUserByIdQuery {
+    override suspend fun execute(dto: FindUserByIdQueryDto): User? = newSuspendedTransaction {
+        Users.select { Users.id eq dto.id }.firstOrNull()?.let(ResultRow::toUser)
+    }
+}
