d5c8a39fbea6df3a820edef61ef9d230ccb00ad0
uramnoil <uramnoil@outlook.jp>
2021-06-15 18:35:00 +0900

    :rotating_light: UUID.fromStringを削除

--- backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/server/UpdateServerCommandImpl.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/server/UpdateServerCommandImpl.kt
@@ -6,14 +6,13 @@ import com.uramnoil.serverist.domain.models.server.*
 import com.uramnoil.serverist.domain.repositories.NotFoundException
 import com.uramnoil.serverist.domain.repositories.ServerRepository
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
-import java.util.*
 
 class UpdateServerCommandImpl(
     private val repository: ServerRepository
 ) : UpdateServerCommand {
     override suspend fun execute(dto: UpdateServerCommandDto) {
         newSuspendedTransaction {
-            val server = repository.findById(Id(UUID.fromString(dto.id)))
+            val server = repository.findById(Id(dto.id))
                 ?: throw NotFoundException("UpdateServerCommand#excecute: サーバー(Id: ${dto.id})が見つかりませんでした。")
             server.apply {
                 name = Name(dto.name)
