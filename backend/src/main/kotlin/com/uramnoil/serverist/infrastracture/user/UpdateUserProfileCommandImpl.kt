77dac44a29581d13c4045ca2d02f616744d8cc2b
uramnoil <uramnoil@outlook.jp>
2021-06-16 00:51:57 +0900

    :rotate_light: 修正

--- backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/user/UpdateUserProfileCommandImpl.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/user/UpdateUserProfileCommandImpl.kt
@@ -2,8 +2,8 @@ package com.uramnoil.serverist.infrastracture.user
 
 import com.uramnoil.serverist.application.user.commands.UpdateUserProfileCommand
 import com.uramnoil.serverist.application.user.commands.UpdateUserProfileCommandDto
+import com.uramnoil.serverist.domain.models.kernel.UserId
 import com.uramnoil.serverist.domain.models.user.Description
-import com.uramnoil.serverist.domain.models.user.Id
 import com.uramnoil.serverist.domain.models.user.Name
 import com.uramnoil.serverist.domain.repositories.NotFoundException
 import com.uramnoil.serverist.domain.repositories.UserRepository
@@ -12,7 +12,7 @@ class UpdateUserProfileCommandImpl(
     private val repository: UserRepository,
 ) : UpdateUserProfileCommand {
     override suspend fun execute(dto: UpdateUserProfileCommandDto) {
-        val user = repository.findById(Id(dto.id))
+        val user = repository.findById(UserId(dto.id))
             ?: throw NotFoundException("UpdateUserCommand#execute: ユーザー(ID: ${dto.id})が見つかりませんでした。")
 
         user.apply {
@@ -20,6 +20,6 @@ class UpdateUserProfileCommandImpl(
             description = Description(dto.description)
         }
 
-        repository.store(user)
+        repository.update(user)
     }
 }
