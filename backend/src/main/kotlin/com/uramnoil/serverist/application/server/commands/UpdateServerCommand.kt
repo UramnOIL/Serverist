57ee247ab7a3f593d76c684d12e4efd5f9111b1e
uramnoil <uramnoil@outlook.jp>
2021-06-15 18:20:58 +0900

    :art: idを追加

--- backend/src/main/kotlin/com/uramnoil/serverist/application/server/commands/UpdateServerCommand.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/application/server/commands/UpdateServerCommand.kt
@@ -1,7 +1,9 @@
 package com.uramnoil.serverist.application.server.commands
 
+import java.util.*
+
 data class UpdateServerCommandDto(
-    val id: String,
+    val id: UUID,
     val name: String,
     val address: String?,
     val port: Int?,
