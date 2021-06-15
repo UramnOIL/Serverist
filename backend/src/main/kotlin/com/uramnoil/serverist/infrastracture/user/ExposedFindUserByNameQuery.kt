3b0095e6ec924a79edd9cafbd937362f785d75ce
uramnoil <uramnoil@outlook.jp>
2021-06-16 00:54:26 +0900

    :art: toApplicationUserに変更

--- backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/user/ExposedFindUserByNameQuery.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/user/ExposedFindUserByNameQuery.kt
@@ -1,38 +1,17 @@
 package com.uramnoil.serverist.infrastracture.user
 
-import com.uramnoil.serverist.application.kernel.User
+import com.uramnoil.serverist.application.user.User
 import com.uramnoil.serverist.application.user.queries.FindUserByNameDto
 import com.uramnoil.serverist.application.user.queries.FindUserByNameQuery
-import com.uramnoil.serverist.domain.services.user.UserFactory
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.lowerCase
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
 class ExposedFindUserByNameQuery : FindUserByNameQuery {
     override suspend fun execute(dto: FindUserByNameDto): User? {
-        val user = newSuspendedTransaction {
-            val result = Users.select { Users.name.lowerCase() eq dto.name.toLowerCase() }.firstOrNull()
-                ?: return@newSuspendedTransaction null
-
-            result.let {
-                UserFactory.create(
-                    it[Users.id].value,
-                    it[Users.accountId],
-                    it[Users.email],
-                    it[Users.hashedPassword],
-                    it[Users.name],
-                    it[Users.description],
-                )
-            }
-        }
-
-        return user?.let {
-            User(
-                id = it.id.value,
-                accountId = it.accountId.value,
-                name = it.name.value,
-                description = it.description.value
-            )
-        }
+        return newSuspendedTransaction {
+            Users.select { Users.name.lowerCase() eq dto.name.toLowerCase() }.firstOrNull()
+        }?.let(ResultRow::toApplicationUser)
     }
 }
