31739d43e551497ea4a409dea170d3620b02566e
uramnoil <uramnoil@outlook.jp>
2021-06-16 00:56:32 +0900

    :fire: 不要

--- backend/src/main/kotlin/com/uramnoil/serverist/domain/repositories/EmailRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.uramnoil.serverist.domain.repositories
-
-import com.uramnoil.serverist.domain.models.user.Id
-import com.uramnoil.serverist.domain.models.user.User
-
-interface EmailRepository {
-    suspend fun store(user: User)
-    suspend fun findById(id: Id): User
-}
