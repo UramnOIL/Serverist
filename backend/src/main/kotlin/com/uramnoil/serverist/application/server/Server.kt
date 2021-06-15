25e458c9cb592102d521110080b95ea0317bf95e
uramnoil <uramnoil@outlook.jp>
2021-06-15 22:36:03 +0900

    :art: UUIDに変更

--- backend/src/main/kotlin/com/uramnoil/serverist/application/server/Server.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/application/server/Server.kt
@@ -1,13 +1,12 @@
 package com.uramnoil.serverist.application.server
 
-import com.uramnoil.serverist.application.kernel.User
 import kotlinx.datetime.Instant
 import java.util.*
 
 data class Server(
     val id: UUID,
     val createdAt: Instant,
-    val owner: User,
+    val owner: UUID,
     val name: String,
     val address: String?,
     val port: Int?,
