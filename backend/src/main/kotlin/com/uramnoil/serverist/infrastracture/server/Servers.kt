b4246244bf96a05fd96b768a4643f3c298f37c28
uramnoil <uramnoil@outlook.jp>
2021-06-15 22:36:21 +0900

    :sparkles: toServerを作成

--- backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/server/Servers.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/server/Servers.kt
@@ -1,10 +1,12 @@
 package com.uramnoil.serverist.infrastracture.server
 
+import com.uramnoil.serverist.application.server.Server
 import com.uramnoil.serverist.infrastracture.user.Users
 import kotlinx.datetime.Instant
 import kotlinx.datetime.toJavaInstant
 import kotlinx.datetime.toKotlinInstant
 import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.`java-time`.datetime
 import java.time.LocalDateTime
 import java.time.ZoneOffset
@@ -17,6 +19,16 @@ object Servers : UUIDTable("servers") {
     val port = integer("port").nullable()
     val description = varchar("description", 255)
 }
+
+fun ResultRow.toServer(): Server = Server(
+    this[Servers.id].value,
+    this[Servers.createdAt].toKotlinInstance(),
+    this[Servers.owner],
+    this[Servers.name],
+    this[Servers.address],
+    this[Servers.port],
+    this[Servers.description]
+)
 
 fun LocalDateTime.toKotlinInstance() = toInstant(ZoneOffset.UTC).toKotlinInstant()
 
