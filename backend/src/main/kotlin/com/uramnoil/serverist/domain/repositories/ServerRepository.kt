c8dcac1fd2d0a6c69638d689dea6fd385647c936
uramnoil <uramnoil@outlook.jp>
2021-06-16 00:22:57 +0900

    :art: 責務を分割

--- backend/src/main/kotlin/com/uramnoil/serverist/domain/repositories/ServerRepository.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/domain/repositories/ServerRepository.kt
@@ -1,9 +1,11 @@
 package com.uramnoil.serverist.domain.repositories
 
+import com.uramnoil.serverist.domain.models.server.Id
 import com.uramnoil.serverist.domain.models.server.Server
 
 interface ServerRepository {
-    suspend fun findById(id: com.uramnoil.serverist.domain.models.server.Id): Server?
-    suspend fun store(server: Server)
+    suspend fun insert(server: Server)
+    suspend fun update(server: Server)
     suspend fun delete(server: Server)
+    suspend fun findById(id: Id): Server?
 }
--- backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/server/ExposedServerRepository.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/server/ExposedServerRepository.kt
@@ -1,7 +1,7 @@
 package com.uramnoil.serverist.infrastracture.server
 
-import com.uramnoil.serverist.domain.models.server.Id
-import com.uramnoil.serverist.domain.models.server.Server
+import com.uramnoil.serverist.domain.models.kernel.UserId
+import com.uramnoil.serverist.domain.models.server.*
 import com.uramnoil.serverist.domain.repositories.ServerRepository
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.select
@@ -10,13 +10,11 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.update
 
 class ExposedServerRepository : ServerRepository {
-    override suspend fun findById(id: Id): Server? = newSuspendedTransaction {
-        val result = Servers.select { Servers.id eq id.value }.firstOrNull() ?: return@newSuspendedTransaction null
-
-        ExposedServerFactory.create(result)
+    override suspend fun insert(server: Server) {
+        TODO("Not yet implemented")
     }
 
-    override suspend fun store(server: Server) = newSuspendedTransaction {
+    override suspend fun update(server: Server) = newSuspendedTransaction {
         Servers.update({ Servers.id eq server.id.value }) {
             it[name] = server.name.value
             it[address] = server.address.value
@@ -30,6 +28,20 @@ class ExposedServerRepository : ServerRepository {
         transaction {
             Servers.deleteWhere { Servers.id eq server.id.value }
             commit()
+        }
+    }
+
+    override suspend fun findById(id: Id): Server? {
+        return Servers.select { Servers.owner eq id.value }.firstOrNull()?.let {
+            Server(
+                Id(it[Servers.id].value),
+                CreatedAt(it[Servers.createdAt].toKotlinInstance()),
+                Name(it[Servers.name]),
+                UserId(it[Servers.owner]),
+                Address(it[Servers.address]),
+                Port(it[Servers.port]),
+                Description(it[Servers.description])
+            )
         }
     }
 }
