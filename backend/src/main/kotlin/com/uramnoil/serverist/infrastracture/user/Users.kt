3a6510628a327a6e71b3ee7584ec355e837daae2
uramnoil <uramnoil@outlook.jp>
2021-06-15 22:30:16 +0900

    :sparkles: toUserを作成

--- backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/user/Users.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/user/Users.kt
@@ -1,6 +1,8 @@
 package com.uramnoil.serverist.infrastracture.user
 
+import com.uramnoil.serverist.application.kernel.User
 import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.ResultRow
 
 object Users : UUIDTable("users") {
     val accountId = varchar("account_id", 16).uniqueIndex()
@@ -9,3 +11,10 @@ object Users : UUIDTable("users") {
     val name = varchar("name", 32)
     val description = varchar("description", 255)
 }
+
+fun ResultRow.toUser() = User(
+    this[Users.id].value,
+    this[Users.accountId],
+    this[Users.name],
+    this[Users.description]
+)
