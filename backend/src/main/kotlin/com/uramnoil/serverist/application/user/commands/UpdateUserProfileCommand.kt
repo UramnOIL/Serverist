82053b4fa9cd9494016045cbb7f876751b47ed34
uramnoil <uramnoil@outlook.jp>
2021-06-15 17:23:06 +0900

    :art: accountIdを追加

--- backend/src/main/kotlin/com/uramnoil/serverist/application/user/commands/UpdateUserProfileCommand.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/application/user/commands/UpdateUserProfileCommand.kt
@@ -2,7 +2,7 @@ package com.uramnoil.serverist.application.user.commands
 
 import java.util.*
 
-data class UpdateUserProfileCommandDto(val id: UUID, val name: String, val description: String)
+data class UpdateUserProfileCommandDto(val id: UUID, val accountId: String, val name: String, val description: String)
 
 interface UpdateUserProfileCommand {
     suspend fun execute(dto: UpdateUserProfileCommandDto)
