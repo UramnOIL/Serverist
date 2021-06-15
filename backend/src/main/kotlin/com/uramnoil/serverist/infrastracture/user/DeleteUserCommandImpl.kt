c633e3f428d4574282f611190301d5a060ff3c49
uramnoil <uramnoil@outlook.jp>
2021-06-16 00:42:05 +0900

    :bug: UserIdに変更

--- backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/user/DeleteUserCommandImpl.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/user/DeleteUserCommandImpl.kt
@@ -2,14 +2,14 @@ package com.uramnoil.serverist.infrastracture.user
 
 import com.uramnoil.serverist.application.user.commands.DeleteUserCommand
 import com.uramnoil.serverist.application.user.commands.DeleteUserCommandDto
-import com.uramnoil.serverist.domain.models.user.Id
+import com.uramnoil.serverist.domain.models.kernel.UserId
 import com.uramnoil.serverist.domain.repositories.NotFoundException
 import com.uramnoil.serverist.domain.repositories.UserRepository
 
 class DeleteUserCommandImpl(private val repository: UserRepository) :
     DeleteUserCommand {
     override suspend fun execute(dto: DeleteUserCommandDto) {
-        val user = repository.findById(Id(dto.id))
+        val user = repository.findById(UserId(dto.id))
             ?: throw NotFoundException("DeleteUserCommand#execute: ユーザー(ID: ${dto.id})が見つかりませんでした。")
 
         repository.delete(user)
