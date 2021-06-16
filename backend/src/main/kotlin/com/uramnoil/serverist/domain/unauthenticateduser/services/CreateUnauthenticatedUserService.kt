5b9694956276fb32b8c3e32d8de164819c71563a
uramnoil <uramnoil@outlook.jp>
2021-06-16 13:45:14 +0900

    :art: 戻り値を追加

--- backend/src/main/kotlin/com/uramnoil/serverist/domain/unauthenticateduser/services/CreateUnauthenticatedUserService.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/domain/unauthenticateduser/services/CreateUnauthenticatedUserService.kt
@@ -3,7 +3,8 @@ package com.uramnoil.serverist.domain.unauthenticateduser.services
 import com.uramnoil.serverist.domain.kernel.models.Password
 import com.uramnoil.serverist.domain.unauthenticateduser.models.AccountId
 import com.uramnoil.serverist.domain.unauthenticateduser.models.Email
+import com.uramnoil.serverist.domain.unauthenticateduser.models.UnauthenticatedUser
 
 interface CreateUnauthenticatedUserService {
-    suspend fun create(userId: AccountId, email: Email, password: Password)
+    suspend fun create(userId: AccountId, email: Email, password: Password): UnauthenticatedUser
 }
