667e761feda18549ec4b38d18c4862e8cf82705a
uramnoil <uramnoil@outlook.jp>
2021-06-16 15:04:27 +0900

    :art: リネーム

--- backend/src/main/kotlin/com/uramnoil/serverist/application/unauthenticateduser/service/SendEmailToAuthenticateService.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/application/unauthenticateduser/service/SendEmailToAuthenticateService.kt
@@ -3,5 +3,5 @@ package com.uramnoil.serverist.application.unauthenticateduser.service
 import com.uramnoil.serverist.domain.unauthenticateduser.models.UnauthenticatedUser
 
 interface SendEmailToAuthenticateService {
-    fun sendTo(user: UnauthenticatedUser)
+    fun execute(user: UnauthenticatedUser)
 }
