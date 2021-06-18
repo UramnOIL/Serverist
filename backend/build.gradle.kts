c64b9b8428af017a7299aaecdec0b538264359de
uramnoil <uramnoil@outlook.jp>
2021-06-19 00:26:05 +0900

    :heavy_sign_plus: spring-boot-starter-data-redisを追加

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -30,30 +30,34 @@ dependencies {
     val kgraphqlVersion: String by project
     val kodeinVersion: String by project
     val exposedVersion: String by project
-    val springSecurityVersion: String by project
     val springBootVersion: String by project
 
-    implementation(kotlin("stdlib", kotlinVersion))
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", coroutinesVersion)
-    implementation("org.jetbrains.kotlinx", "kotlinx-datetime", datetimeVersion)
-    implementation("io.ktor", "ktor-server-netty", ktorVersion)
-    implementation("io.ktor", "ktor-auth", ktorVersion)
-    implementation("io.ktor", "ktor-auth-jwt", ktorVersion)
-    implementation("ch.qos.logback", "logback-classic", logbackVersion)
-    implementation("com.apurebase", "kgraphql", kgraphqlVersion)
-    implementation("com.apurebase", "kgraphql-ktor", kgraphqlVersion)
-    implementation("org.kodein.di", "kodein-di-jvm", kodeinVersion)
-    implementation("org.jetbrains.exposed", "exposed-core", exposedVersion)
-    implementation("org.jetbrains.exposed", "exposed-dao", exposedVersion)
-    implementation("org.jetbrains.exposed", "exposed-jdbc", exposedVersion)
-    implementation("org.jetbrains.exposed", "exposed-java-time", exposedVersion)
-    implementation("mysql", "mysql-connector-java", "8.0.20")
-    implementation("org.springframework.security", "spring-security-core", springSecurityVersion)
-    implementation("org.springframework.boot", "spring-boot-starter-mail", springBootVersion)
+    implementation(kotlin("stdlib:$kotlinVersion"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:$datetimeVersion")
 
-    testImplementation("io.ktor", "ktor-server-tests", ktorVersion)
-    testImplementation("io.ktor", "ktor-server-test-host", ktorVersion)
-    testImplementation("com.h2database", "h2", "1.4.200")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-auth:$ktorVersion")
+    implementation("io.ktor:ktor-auth-jwt:$ktorVersion")
+    implementation("com.apurebase:kgraphql:$kgraphqlVersion")
+    implementation("com.apurebase:kgraphql-ktor:$kgraphqlVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    implementation("org.kodein.di:kodein-di-jvm:$kodeinVersion")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+    implementation("mysql:mysql-connector-java:8.0.20")
+
+    implementation("org.springframework.boot:spring-boot-starter-security:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-mail:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis:$springBootVersion")
+
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
+    testImplementation("com.h2database:h2:1.4.200")
 }
 
 tasks.withType<Jar> {
