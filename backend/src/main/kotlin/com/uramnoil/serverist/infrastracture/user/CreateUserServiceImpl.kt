16e62fa03778f636726998b9d70257bcb0b6c1c5
uramnoil <uramnoil@outlook.jp>
2021-06-20 03:17:35 +0900

    :rotate_light: DTOを使用するように変更

--- backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/user/CreateUserServiceImpl.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/user/CreateUserServiceImpl.kt
@@ -1,26 +1,24 @@
 package com.uramnoil.serverist.infrastracture.user
 
-import com.uramnoil.serverist.domain.user.models.*
+import com.uramnoil.serverist.domain.kernel.models.UserId
+import com.uramnoil.serverist.domain.user.models.User
 import com.uramnoil.serverist.domain.user.repositories.UserRepository
 import com.uramnoil.serverist.domain.user.services.CreateUserService
+import com.uramnoil.serverist.domain.user.services.CreateUserServiceDto
 import java.util.*
 
 class CreateUserServiceImpl(private val repository: UserRepository) : CreateUserService {
-    override suspend fun new(
-        accountId: AccountId,
-        email: Email,
-        hashedPassword: com.uramnoil.serverist.domain.kernel.models.HashedPassword,
-        name: Name,
-        description: Description
-    ): User {
-        val user = User(
-            id = com.uramnoil.serverist.domain.kernel.models.UserId(UUID.randomUUID()),
-            accountId = accountId,
-            email = email,
-            hashedPassword = hashedPassword,
-            name = name,
-            description = description
-        )
+    override suspend fun new(dto: CreateUserServiceDto): User {
+        val user = dto.run {
+            User(
+                id = UserId(UUID.randomUUID()),
+                accountId = accountId,
+                email = email,
+                hashedPassword = hashedPassword,
+                name = name,
+                description = description
+            )
+        }
         repository.insert(user)
 
         return user
