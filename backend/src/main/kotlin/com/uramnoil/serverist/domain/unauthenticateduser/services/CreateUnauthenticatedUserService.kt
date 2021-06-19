806a618f9d6fea8721b4b928a22a3e01bf5c959f
uramnoil <uramnoil@outlook.jp>
2021-06-20 03:19:30 +0900

    :tag: HashedPasswordに変更

--- backend/src/main/kotlin/com/uramnoil/serverist/domain/unauthenticateduser/services/CreateUnauthenticatedUserService.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/domain/unauthenticateduser/services/CreateUnauthenticatedUserService.kt
@@ -1,11 +1,15 @@
 package com.uramnoil.serverist.domain.unauthenticateduser.services
 
-import com.uramnoil.serverist.domain.kernel.models.Password
+import com.uramnoil.serverist.domain.kernel.models.HashedPassword
 import com.uramnoil.serverist.domain.unauthenticateduser.models.AccountId
 import com.uramnoil.serverist.domain.unauthenticateduser.models.Email
 import com.uramnoil.serverist.domain.unauthenticateduser.models.UnauthenticatedUser
 
-data class CreateUnauthenticatedUserServiceDto(val accountId: AccountId, val email: Email, val password: Password)
+data class CreateUnauthenticatedUserServiceDto(
+    val accountId: AccountId,
+    val email: Email,
+    val hashedPassword: HashedPassword
+)
 
 interface CreateUnauthenticatedUserService {
     suspend fun execute(dto: CreateUnauthenticatedUserServiceDto): UnauthenticatedUser
