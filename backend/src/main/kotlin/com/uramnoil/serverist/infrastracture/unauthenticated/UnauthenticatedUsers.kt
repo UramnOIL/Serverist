4e8b2a90dfb83930a75cb147ecc28f912fd0f476
uramnoil <uramnoil@outlook.jp>
2021-06-20 02:37:11 +0900

    :sparkles: UnauthenticatedUsersを作成

---
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/unauthenticated/UnauthenticatedUsers.kt
@@ -0,0 +1,11 @@
+package com.uramnoil.serverist.infrastracture.unauthenticated
+
+import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.`java-time`.datetime
+
+object UnauthenticatedUsers : UUIDTable("users") {
+    val accountId = varchar("account_id", 16).uniqueIndex()
+    val email = varchar("email", 255).uniqueIndex()
+    val hashedPassword = varchar("hashed_password", 255)
+    val expiredDateTime = datetime("expired_datetime")
+}
