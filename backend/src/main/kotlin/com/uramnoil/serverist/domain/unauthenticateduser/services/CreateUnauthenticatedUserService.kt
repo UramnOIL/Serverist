e791bb662a64653b6facabd87ca9205c8a0cdcf2
uramnoil <uramnoil@outlook.jp>
2021-06-20 03:17:00 +0900

    :art: accountIdに変更

--- backend/src/main/kotlin/com/uramnoil/serverist/domain/unauthenticateduser/services/CreateUnauthenticatedUserService.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/domain/unauthenticateduser/services/CreateUnauthenticatedUserService.kt
@@ -5,7 +5,7 @@ import com.uramnoil.serverist.domain.unauthenticateduser.models.AccountId
 import com.uramnoil.serverist.domain.unauthenticateduser.models.Email
 import com.uramnoil.serverist.domain.unauthenticateduser.models.UnauthenticatedUser
 
-data class CreateUnauthenticatedUserServiceDto(val userId: AccountId, val email: Email, val password: Password)
+data class CreateUnauthenticatedUserServiceDto(val accountId: AccountId, val email: Email, val password: Password)
 
 interface CreateUnauthenticatedUserService {
     suspend fun execute(dto: CreateUnauthenticatedUserServiceDto): UnauthenticatedUser
