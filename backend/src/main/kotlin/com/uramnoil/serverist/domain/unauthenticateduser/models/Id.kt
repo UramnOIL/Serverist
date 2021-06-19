ec7ef1fb8339fabd02b6c0b3893dad6621f8d1d2
uramnoil <uramnoil@outlook.jp>
2021-06-20 02:44:56 +0900

    :sparkles: Idを作成

---
+++ backend/src/main/kotlin/com/uramnoil/serverist/domain/unauthenticateduser/models/Id.kt
@@ -0,0 +1,5 @@
+package com.uramnoil.serverist.domain.unauthenticateduser.models
+
+import java.util.*
+
+data class Id(val value: UUID)
--- backend/src/main/kotlin/com/uramnoil/serverist/domain/unauthenticateduser/models/UnauthenticatedUser.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/domain/unauthenticateduser/models/UnauthenticatedUser.kt
@@ -1,8 +1,8 @@
 package com.uramnoil.serverist.domain.unauthenticateduser.models
 
 class UnauthenticatedUser(
+    val id: Id,
     val accountId: AccountId,
     val email: Email,
     val hashedPassword: com.uramnoil.serverist.domain.kernel.models.HashedPassword
-) {
-}
+)
