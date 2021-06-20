a5683b814653c5aa23f3ff31559c0f28741eb6ae
uramnoil <uramnoil@outlook.jp>
2021-06-20 18:39:44 +0900

    :rotating_light: repositoryに変更

--- backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/user/CreateUserCommandImpl.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/user/CreateUserCommandImpl.kt
@@ -1,15 +1,20 @@
 package com.uramnoil.serverist.infrastracture.user
 
 import com.uramnoil.serverist.application.user.commands.CreateUserCommand
+import com.uramnoil.serverist.application.user.toApplication
 import com.uramnoil.serverist.domain.kernel.models.user.AccountId
 import com.uramnoil.serverist.domain.kernel.models.user.Email
 import com.uramnoil.serverist.domain.kernel.models.user.HashedPassword
+import com.uramnoil.serverist.domain.kernel.models.user.UserId
 import com.uramnoil.serverist.domain.user.models.Description
 import com.uramnoil.serverist.domain.user.models.Name
-import com.uramnoil.serverist.domain.user.services.CreateUserService
+import com.uramnoil.serverist.domain.user.repositories.UserRepository
+import java.util.*
+import com.uramnoil.serverist.application.user.User as ApplicationUser
+import com.uramnoil.serverist.domain.user.models.User as DomainUser
 
 class CreateUserCommandImpl(
-    private val createUserService: CreateUserService
+    private val repository: UserRepository
 ) : CreateUserCommand {
     override suspend fun execute(
         accountId: String,
@@ -17,13 +22,18 @@ class CreateUserCommandImpl(
         hashedPassword: String,
         name: String,
         description: String
-    ) {
-        createUserService.new(
+    ): ApplicationUser {
+        val user = DomainUser(
+            UserId(UUID.randomUUID()),
             AccountId(accountId),
             Email(email),
             HashedPassword(hashedPassword),
             Name(name),
             Description(description)
         )
+
+        repository.insert(user)
+
+        return user.toApplication()
     }
 }
