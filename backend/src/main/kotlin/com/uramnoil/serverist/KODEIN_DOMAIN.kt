3b805c5ea80aaac60f9207d0f430aa512a94b5bd
uramnoil <uramnoil@outlook.jp>
2021-06-20 18:19:34 +0900

    :fire: CreateServiceを削除

--- backend/src/main/kotlin/com/uramnoil/serverist/KODEIN_DOMAIN.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/KODEIN_DOMAIN.kt
@@ -1,21 +1,14 @@
 package com.uramnoil.serverist
 
 import com.uramnoil.serverist.domain.server.repositories.ServerRepository
-import com.uramnoil.serverist.domain.server.services.CreateServerService
-import com.uramnoil.serverist.domain.unauthenticateduser.services.CreateUnauthenticatedUserService
 import com.uramnoil.serverist.domain.user.repositories.UserRepository
-import com.uramnoil.serverist.domain.user.services.CreateUserService
 import com.uramnoil.serverist.infrastracture.HashPasswordServiceImpl
-import com.uramnoil.serverist.infrastracture.server.CreateServerServiceImpl
 import com.uramnoil.serverist.infrastracture.server.ExposedServerRepository
-import com.uramnoil.serverist.infrastracture.unauthenticateduser.CreateUnauthenticatedUserServiceImpl
-import com.uramnoil.serverist.infrastracture.user.CreateUserServiceImpl
 import com.uramnoil.serverist.infrastracture.user.ExposedUserRepository
 import io.ktor.application.*
 import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.factory
-import org.kodein.di.instance
 
 fun ApplicationEnvironment.buildDomainDi(di: DI) = DI {
     this.extend(di)
@@ -25,24 +18,12 @@ fun ApplicationEnvironment.buildDomainDi(di: DI) = DI {
         ExposedUserRepository()
     }
 
-    bind<CreateUserService>() with factory {
-        CreateUserServiceImpl(instance())
-    }
-
     // <-- UnapprovedUser -->
-
-    bind<CreateUnauthenticatedUserService>() with factory {
-        CreateUnauthenticatedUserServiceImpl(instance())
-    }
 
     // <-- Server -->
 
     bind<ServerRepository>() with factory {
         ExposedServerRepository()
-    }
-
-    bind<CreateServerService>() with factory {
-        CreateServerServiceImpl(instance())
     }
 
     // <-- Password -->
