05d15a7350935d705de3f1509d3d0627977b1f91
uramnoil <uramnoil@outlook.jp>
2021-06-21 01:29:37 +0900

    :rotating_light: DTOを削除

--- backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/server/UpdateServerCommandImpl.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastracture/server/UpdateServerCommandImpl.kt
@@ -1,24 +1,24 @@
 package com.uramnoil.serverist.infrastracture.server
 
 import com.uramnoil.serverist.application.server.commands.UpdateServerCommand
-import com.uramnoil.serverist.application.server.commands.UpdateServerCommandDto
 import com.uramnoil.serverist.domain.kernel.NotFoundException
 import com.uramnoil.serverist.domain.server.models.*
 import com.uramnoil.serverist.domain.server.repositories.ServerRepository
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import java.util.*
 
 class UpdateServerCommandImpl(
     private val repository: ServerRepository
 ) : UpdateServerCommand {
-    override suspend fun execute(dto: UpdateServerCommandDto) {
+    override suspend fun execute(id: UUID, name: String, address: String?, port: Int?, description: String) {
         newSuspendedTransaction {
-            val server = repository.findById(Id(dto.id))
-                ?: throw NotFoundException("UpdateServerCommand#excecute: サーバー(Id: ${dto.id})が見つかりませんでした。")
+            val server = repository.findById(Id(id))
+                ?: throw NotFoundException("UpdateServerCommand#excecute: サーバー(Id: ${id})が見つかりませんでした。")
             server.apply {
-                name = Name(dto.name)
-                address = Address(dto.address)
-                port = Port(dto.port)
-                description = Description(dto.description)
+                this.name = Name(name)
+                this.address = Address(address)
+                this.port = Port(port)
+                this.description = Description(description)
             }
             repository.update(server)
             commit()
