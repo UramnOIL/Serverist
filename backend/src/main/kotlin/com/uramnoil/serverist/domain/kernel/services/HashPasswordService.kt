63d826c413fa28e4a5d33e53625ecbfadc29abc8
uramnoil <uramnoil@outlook.jp>
2021-06-20 18:24:08 +0900

    :tag: HashedPasswordに変更

--- backend/src/main/kotlin/com/uramnoil/serverist/domain/kernel/services/HashPasswordService.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/domain/kernel/services/HashPasswordService.kt
@@ -4,7 +4,7 @@ import com.uramnoil.serverist.domain.kernel.models.user.HashedPassword
 import com.uramnoil.serverist.domain.kernel.models.user.Password
 
 interface HashPasswordService {
-    fun hash(password: Password): String
+    fun hash(password: Password): HashedPassword
     fun check(
         password: Password,
         hashedPassword: HashedPassword
