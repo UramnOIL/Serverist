b01ce89f7afe7e1c2d411c59a9aa6fe1eed9dcbc
uramnoil <uramnoil@outlook.jp>
2021-07-21 13:16:51 +0900

    :heavy_sign_plus: benasher44/uuidを追加

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -9,13 +9,15 @@ kotlin {
 
     sourceSets {
         val kotlinVersion: String by project
-        val kodeinVersion: String by project
         val coroutinesVersion: String by project
+        val uuidVersion: String by project
+
+        println(uuidVersion)
 
         val commonMain by getting {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-                implementation("org.kodein.di:kodein-di-jvm:$kodeinVersion")
+                implementation("com.benasher44:uuid:${uuidVersion}")
             }
         }
     }
