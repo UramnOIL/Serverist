2518bc897258a9ca79d60c4d4df093b549812458
uramnoil <uramnoil@outlook.jp>
2021-07-26 03:47:40 +0900

    :fire: メソッドを削除

--- application/src/commonMain/kotlin/com/uramnoil/serverist/application/unauthenticateduser/service/SendEmailToAuthenticateService.kt
+++ application/src/commonMain/kotlin/com/uramnoil/serverist/application/unauthenticateduser/service/SendEmailToAuthenticateService.kt
@@ -1,7 +1,3 @@
 package com.uramnoil.serverist.application.unauthenticateduser.service
 
-import com.uramnoil.serverist.application.unauthenticateduser.UnauthenticatedUser
-
-expect interface SendEmailToAuthenticateService {
-    fun execute(user: UnauthenticatedUser)
-}
+expect interface SendEmailToAuthenticateService
