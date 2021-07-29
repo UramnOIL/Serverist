1dac9d31532b518f824f469f144da9c68fb160ee
uramnoil <uramnoil@outlook.jp>
2021-07-29 16:20:27 +0900

    :art: productModuleに変更

--- backend/src/main/kotlin/com/uramnoil/serverist/Application.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/Application.kt
@@ -34,8 +34,7 @@ data class AuthSession(val id: Uuid) : Principal
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused")
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.productModule() {
     val di = environment.buildApplicationDi(environment.buildDomainDi(DI {}))
 
     createConnection()
