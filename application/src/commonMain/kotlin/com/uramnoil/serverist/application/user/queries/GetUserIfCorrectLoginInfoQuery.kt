ac1a9f42a86e8d16045e050a573df7a8921388a7
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-10-17 17:46:00 +0900

    :sparkles: GetUserIfCorrectLoginInfoQueryOutputPort

--- application/src/commonMain/kotlin/com/uramnoil/serverist/application/user/queries/GetUserIfCorrectLoginInfoQuery.kt
+++ application/src/commonMain/kotlin/com/uramnoil/serverist/application/user/queries/GetUserIfCorrectLoginInfoQuery.kt
@@ -3,5 +3,9 @@ package com.uramnoil.serverist.application.user.queries
 import com.uramnoil.serverist.application.user.User
 
 interface GetUserIfCorrectLoginInfoQueryInputPort {
+    fun execute(accountIdOrEmail: String, password: String)
+}
+
+interface GetUserIfCorrectLoginInfoQueryOutputPort {
     fun handle(result: Result<User?>)
 }
