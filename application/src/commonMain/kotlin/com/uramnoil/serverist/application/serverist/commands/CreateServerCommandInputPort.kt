7da38b2229db951092be81bb124aca1ec4814afb
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-10-17 19:45:02 +0900

    :memo: ドキュメントを追加

--- application/src/commonMain/kotlin/com/uramnoil/serverist/application/serverist/commands/CreateServerCommandInputPort.kt
+++ application/src/commonMain/kotlin/com/uramnoil/serverist/application/serverist/commands/CreateServerCommandInputPort.kt
@@ -3,16 +3,36 @@ package com.uramnoil.serverist.application.serverist.commands
 import com.benasher44.uuid.Uuid
 import com.uramnoil.serverist.application.serverist.Server
 
+/**
+ * A input-port of CreateServerCommandUseCase.
+ */
 interface CreateServerCommandInputPort {
+    /**
+     * Request to create a new server.
+     *
+     * @param ownerId       server owner's UUID
+     * @param name          the name of the new server
+     * @param host          the host of the new server
+     * @param port          the port of the new server
+     * @param description   the description of the new server
+     */
     fun execute(
         ownerId: Uuid,
         name: String,
-        address: String?,
+        host: String?,
         port: Int?,
         description: String
     )
 }
 
+/**
+ * A output-port of CreateServerCommandUseCase
+ */
 interface CreateServerCommandOutputPort {
+    /**
+     * Handle result of request of creating a new server.
+     *
+     * @param result    the result of the request.
+     */
     fun handle(result: Result<Server>)
 }
