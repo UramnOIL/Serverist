07c71b89ea59e4c55e52d5edbff91ca93df41f17
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-10-17 20:06:55 +0900

    :art: 名前にUsecaseを追加

--- application/src/commonMain/kotlin/com/uramnoil/serverist/application/server/commands/DeleteServerCommandUseCase.kt
+++ application/src/commonMain/kotlin/com/uramnoil/serverist/application/server/commands/DeleteServerCommandUseCase.kt
@@ -2,10 +2,10 @@ package com.uramnoil.serverist.application.server.commands
 
 import com.benasher44.uuid.Uuid
 
-interface DeleteServerCommandInputPort {
+interface DeleteServerCommandUseCaseInputPort {
     fun execute(id: Uuid)
 }
 
-interface DeleteServerCommandOutputPort {
+interface DeleteServerCommandUseCaseOutputPort {
     fun handle(result: Result<Unit>)
 }
