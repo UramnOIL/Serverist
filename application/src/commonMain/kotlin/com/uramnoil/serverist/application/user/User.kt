cfecc8eb2ff13cf78617485eb68a3ed047ece0db
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-10-18 00:24:53 +0900

    :fire: DomainUser.toApplication()

--- application/src/commonMain/kotlin/com/uramnoil/serverist/application/user/User.kt
+++ application/src/commonMain/kotlin/com/uramnoil/serverist/application/user/User.kt
@@ -1,18 +1,10 @@
 package com.uramnoil.serverist.application.user
 
 import com.benasher44.uuid.Uuid
-import com.uramnoil.serverist.domain.user.models.User as DomainUser
 
 data class User(
     val id: Uuid,
     val accountId: String,
     val name: String,
     val description: String
-)
-
-fun DomainUser.toApplication() = User(
-    id = id.value,
-    accountId = accountId.value,
-    name = name.value,
-    description = description.value
 )
