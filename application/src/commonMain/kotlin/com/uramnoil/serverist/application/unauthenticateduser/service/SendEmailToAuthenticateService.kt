4a8236e1e6e56e87d96b26bf45eb8e836ac66330
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-10-18 00:23:56 +0900

    :fire: OutputPort

--- application/src/commonMain/kotlin/com/uramnoil/serverist/application/unauthenticateduser/service/SendEmailToAuthenticateService.kt
+++ application/src/commonMain/kotlin/com/uramnoil/serverist/application/unauthenticateduser/service/SendEmailToAuthenticateService.kt
@@ -3,9 +3,5 @@ package com.uramnoil.serverist.application.unauthenticateduser.service
 import com.uramnoil.serverist.application.unauthenticateduser.UnauthenticatedUser
 
 interface SendEmailToAuthenticateService {
-    fun execute(user: UnauthenticatedUser)
-}
-
-interface SendEmailToAuthenticateServiceOutputPort {
-    fun handle(result: Result<Unit>)
+    fun execute(user: UnauthenticatedUser): Result<Unit>
 }
