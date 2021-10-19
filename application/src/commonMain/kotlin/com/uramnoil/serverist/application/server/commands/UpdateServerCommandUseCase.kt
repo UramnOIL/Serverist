dd7f4ace505c74d7cab37751c892f6cf86f51d6a
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-10-19 20:27:54 +0900

    :pencil2: UseCaseを追加

--- application/src/commonMain/kotlin/com/uramnoil/serverist/application/server/commands/UpdateServerCommandUseCase.kt
+++ application/src/commonMain/kotlin/com/uramnoil/serverist/application/server/commands/UpdateServerCommandUseCase.kt
@@ -3,7 +3,7 @@ package com.uramnoil.serverist.application.server.commands
 import com.benasher44.uuid.Uuid
 
 
-interface UpdateServerCommandInputPort {
+interface UpdateServerCommandUseCaseInputPort {
     fun execute(
         id: Uuid,
         name: String,
@@ -13,6 +13,6 @@ interface UpdateServerCommandInputPort {
     )
 }
 
-fun interface UpdateServerCommandOutputPort {
+fun interface UpdateServerCommandUseCaseOutputPort {
     fun handle(result: Result<Unit>)
 }
