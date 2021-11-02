b3c09f0e69ed6433abe86ee7e0811275b5fd791d
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-02 23:57:04 +0900

    :sparkles: AuthController

---
+++ backend/src/main/kotlin/com/uramnoil/serverist/presenter/AuthController.kt
@@ -0,0 +1,22 @@
+package com.uramnoil.serverist.presenter
+
+import com.uramnoil.serverist.application.auth.authenticated.TryLoginUseCaseInputPort
+import com.uramnoil.serverist.application.auth.unauthenticated.commands.CreateUserCommandUseCaseInputPort
+import java.util.*
+
+class AuthController(
+    private val createUserCommandUseCaseInputPort: CreateUserCommandUseCaseInputPort,
+    private val tryLoginUseCaseInputPort: TryLoginUseCaseInputPort,
+) {
+    suspend fun signUp(email: String, password: String): Result<Unit> {
+        return createUserCommandUseCaseInputPort.execute(email, password)
+    }
+
+    suspend fun login(email: String, password: String): Result<UUID?> {
+        return tryLoginUseCaseInputPort.execute(email, password)
+    }
+
+    suspend fun activate(token: String): Result<Unit> {
+        TODO("後で実装")
+    }
+}
