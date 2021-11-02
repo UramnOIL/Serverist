26685781f5a6457a0a2e0ec5174f8f4c71483848
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-03 00:31:05 +0900

    :art: CoroutineScopeを削除

--- backend/src/main/kotlin/com/uramnoil/serverist/infrastructure/auth/unauthenticated/application/queries/FindUserByIdQueryUseCaseInteractor.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastructure/auth/unauthenticated/application/queries/FindUserByIdQueryUseCaseInteractor.kt
@@ -6,17 +6,11 @@ import com.uramnoil.serverist.application.auth.unauthenticated.queries.User
 import com.uramnoil.serverist.domain.auth.unauthenticated.models.Id
 import com.uramnoil.serverist.domain.auth.unauthenticated.repositories.UserRepository
 import com.uramnoil.serverist.infrastructure.auth.unauthenticated.toApplication
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.job
-import kotlin.coroutines.CoroutineContext
 
 class FindUserByIdQueryUseCaseInteractor(
     private val repository: UserRepository,
-    parentContext: CoroutineContext
 ) :
-    FindUserByIdQueryUseCaseInputPort,
-    CoroutineScope by CoroutineScope(parentContext + Job(parentContext.job)) {
+    FindUserByIdQueryUseCaseInputPort {
     override suspend fun execute(id: Uuid): Result<User?> {
         val result = repository.findById(Id(id)).map {
             it?.toApplication()
