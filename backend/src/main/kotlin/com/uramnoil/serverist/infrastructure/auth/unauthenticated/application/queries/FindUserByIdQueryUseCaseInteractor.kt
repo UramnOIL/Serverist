8a7704ab79cafc4abb07731b63e78f9fa587b86d
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-03 00:28:42 +0900

    :bug: 修正漏れ

--- backend/src/main/kotlin/com/uramnoil/serverist/infrastructure/auth/unauthenticated/application/queries/FindUserByIdQueryUseCaseInteractor.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/infrastructure/auth/unauthenticated/application/queries/FindUserByIdQueryUseCaseInteractor.kt
@@ -1,30 +1,26 @@
-package com.uramnoil.serverist.infrastracture.auth.unauthenticated.application.queries
+package com.uramnoil.serverist.infrastructure.auth.unauthenticated.application.queries
 
 import com.benasher44.uuid.Uuid
 import com.uramnoil.serverist.application.auth.unauthenticated.queries.FindUserByIdQueryUseCaseInputPort
-import com.uramnoil.serverist.application.auth.unauthenticated.queries.FindUserByIdQueryUseCaseOutputPort
+import com.uramnoil.serverist.application.auth.unauthenticated.queries.User
 import com.uramnoil.serverist.domain.auth.unauthenticated.models.Id
 import com.uramnoil.serverist.domain.auth.unauthenticated.repositories.UserRepository
 import com.uramnoil.serverist.infrastructure.auth.unauthenticated.toApplication
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.job
-import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 class FindUserByIdQueryUseCaseInteractor(
-    private val outputPort: FindUserByIdQueryUseCaseOutputPort,
     private val repository: UserRepository,
     parentContext: CoroutineContext
 ) :
     FindUserByIdQueryUseCaseInputPort,
     CoroutineScope by CoroutineScope(parentContext + Job(parentContext.job)) {
-    override fun execute(id: Uuid) {
-        launch {
-            val result = repository.findById(Id(id)).map {
-                it?.toApplication()
-            }
-            outputPort.handle(result)
+    override suspend fun execute(id: Uuid): Result<User?> {
+        val result = repository.findById(Id(id)).map {
+            it?.toApplication()
         }
+        return result
     }
 }
