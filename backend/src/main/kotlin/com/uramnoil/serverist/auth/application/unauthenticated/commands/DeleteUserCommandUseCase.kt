b9a1033da29e954dcf1bb93a9a846843afc42ff5
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-03 23:16:38 +0900

    :art: UUIDに変更

--- backend/src/main/kotlin/com/uramnoil/serverist/auth/application/unauthenticated/commands/DeleteUserCommandUseCase.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/auth/application/unauthenticated/commands/DeleteUserCommandUseCase.kt
@@ -1,7 +1,8 @@
 package com.uramnoil.serverist.auth.application.unauthenticated.commands
 
-import com.benasher44.uuid.Uuid
+import java.util.*
+
 
 interface DeleteUserCommandInputPort {
-    suspend fun execute(id: Uuid): Result<Unit>
+    suspend fun execute(id: UUID): Result<Unit>
 }
