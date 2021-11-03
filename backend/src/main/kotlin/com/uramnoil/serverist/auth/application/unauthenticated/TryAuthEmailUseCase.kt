76ea614f5a64ceea4297e0d6660373c143dc842c
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-03 10:14:45 +0900

    :fire: TryAuthEmailUseCase

--- backend/src/main/kotlin/com/uramnoil/serverist/auth/application/unauthenticated/TryAuthEmailUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.uramnoil.serverist.auth.application.unauthenticated
-
-import com.benasher44.uuid.Uuid
-
-
-interface TryAuthEmailUseCaseInputPort {
-    suspend fun execute(token: Uuid): Result<Unit>
-}
