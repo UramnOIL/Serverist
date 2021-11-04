83f81e6a868c8b4905bcca0c9f2a81a8f78a5084
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-04 09:34:08 +0900

    :truck: DeleteUserCommandInputPort -> DeleteUserCommandUseCaseInputPort

--- backend/src/main/kotlin/com/uramnoil/serverist/auth/application/unauthenticated/commands/DeleteUserCommandUseCase.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/auth/application/unauthenticated/commands/DeleteUserCommandUseCase.kt
@@ -3,6 +3,6 @@ package com.uramnoil.serverist.auth.application.unauthenticated.commands
 import java.util.*
 
 
-interface DeleteUserCommandInputPort {
+interface DeleteUserCommandUseCaseInputPort {
     suspend fun execute(id: UUID): Result<Unit>
 }
--- backend/src/main/kotlin/com/uramnoil/serverist/auth/infrastructure/unauthenticated/application/commands/DeleteUserCommandUseCaseInteractor.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/auth/infrastructure/unauthenticated/application/commands/DeleteUserCommandUseCaseInteractor.kt
@@ -1,13 +1,13 @@
 package com.uramnoil.serverist.auth.infrastructure.unauthenticated.application.commands
 
 import com.benasher44.uuid.Uuid
-import com.uramnoil.serverist.auth.application.unauthenticated.commands.DeleteUserCommandInputPort
+import com.uramnoil.serverist.auth.application.unauthenticated.commands.DeleteUserCommandUseCaseInputPort
 import com.uramnoil.serverist.domain.auth.unauthenticated.models.Id
 import com.uramnoil.serverist.domain.auth.unauthenticated.repositories.UserRepository
 
 class DeleteUserCommandUseCaseInteractor(
     private val repository: UserRepository,
-) : DeleteUserCommandInputPort {
+) : DeleteUserCommandUseCaseInputPort {
     override suspend fun execute(id: Uuid): Result<Unit> {
         val findResult = repository.findById(Id(id)).mapCatching {
             it ?: throw IllegalArgumentException("id=${id}に一致するユーザーが見つかりませんでした。")
