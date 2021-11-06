9a092197f8a03b3e603ab67c9f03a165ea6b3dc6
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-06 21:37:14 +0900

    :sparkles: `routing/Auth`に認証処理関連のルーティングを移動

--- backend/src/main/kotlin/com/uramnoil/serverist/Application.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/Application.kt
@@ -2,13 +2,9 @@ package com.uramnoil.serverist
 
 import com.apurebase.kgraphql.GraphQL
 import com.benasher44.uuid.Uuid
-import com.uramnoil.serverist.auth.application.unauthenticated.commands.AccountAlreadyExistsException
-import com.uramnoil.serverist.auth.application.unauthenticated.commands.VerificationCodeHasAlreadyBeenSentException
-import com.uramnoil.serverist.domain.common.exception.NotFoundException
 import com.uramnoil.serverist.graphql.PageRequest
 import com.uramnoil.serverist.graphql.serverSchema
 import com.uramnoil.serverist.graphql.userSchema
-import com.uramnoil.serverist.presenter.AuthController
 import com.uramnoil.serverist.presenter.ServerController
 import com.uramnoil.serverist.presenter.UserController
 import com.uramnoil.serverist.serverist.server.application.queries.OrderBy
@@ -21,10 +17,8 @@ import io.ktor.features.ContentTransformationException
 import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
-import io.ktor.routing.*
 import io.ktor.serialization.*
 import io.ktor.sessions.*
-import io.ktor.util.*
 import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
@@ -32,8 +26,8 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import org.koin.ktor.ext.Koin
 import org.koin.ktor.ext.inject
 import org.slf4j.event.Level
+import routing.routingAuth
 import java.io.File
-import java.util.*
 
 @Serializable
 data class AuthSession(val id: Uuid) : Principal
@@ -112,120 +106,7 @@ fun Application.createConnection() {
     }
 }
 
-/**
- * 認証関連のルーティング
- */
-fun Application.routingAuth() = routing {
-    val controller: AuthController by inject()
-    post("login") {
-        @Serializable
-        data class CredentialParameters(val email: String, val password: String)
 
-        val (email, password) = call.receive<CredentialParameters>()
-
-        // すでにログイン済みだった場合
-        if (call.sessions.get<AuthSession>() != null) {
-            call.respond(HttpStatusCode.OK)
-            return@post
-        }
-
-        // ログイン処理
-        val loginResult = controller.login(email, password)
-
-        val id = loginResult.getOrElse {
-            // サーバーエラー
-            call.respond(HttpStatusCode.InternalServerError)
-            return@post
-        }
-
-        id ?: run {
-            // 不正なクレデンシャル
-            call.respond(HttpStatusCode.BadRequest)
-            return@post
-        }
-
-        call.sessions.set(AuthSession(id))
-    }
-
-    // 登録
-    post("signup") {
-        @Serializable
-        data class EmailAndPassword(val email: String, val password: String)
-
-        val (email, password) = call.receive<EmailAndPassword>()
-
-        // サインアップ処理
-        val createUserResult = controller.signUp(email, password)
-
-        // 成功時
-        createUserResult.onSuccess {
-            call.respond(HttpStatusCode.OK)
-        }
-
-        // 失敗時
-        createUserResult.onFailure {
-            when (it) {
-                // コードをすでに送信済み
-                is VerificationCodeHasAlreadyBeenSentException -> {
-                    call.respond(
-                        HttpStatusCode.BadRequest,
-                        "The email owner must have already been received a code to verify."
-                    )
-                }
-                // Emailがすでに使われている
-                is AccountAlreadyExistsException -> {
-                    call.respond(HttpStatusCode.BadRequest, "This email has already been used.")
-                }
-                // サーバーエラー
-                else -> {
-                    call.respond(HttpStatusCode.InternalServerError)
-                    log.error(it)
-                }
-            }
-        }
-    }
-
-    // Email認証
-    get("activate") {
-        @Serializable
-        data class AuthenticateUserId(val id: UUID)
-
-        val (id) = call.receive<AuthenticateUserId>()
-
-        val result = controller.activate(id)
-
-        result.fold(
-            {
-                HttpStatusCode.OK
-            },
-            {
-                when (it) {
-                    is NotFoundException -> {
-                        log.debug(it.message)
-                        HttpStatusCode.BadRequest
-                    }
-                    else -> {
-                        log.error(it)
-                        HttpStatusCode.InternalServerError
-                    }
-                }
-            }
-        )
-    }
-
-    post("activate/resend") {
-        call.respond(HttpStatusCode.InternalServerError)
-    }
-
-    post("withdrawal") {
-        val auth = call.sessions.get<AuthSession>()
-        if (auth == null) {
-            call.respond(HttpStatusCode.OK)
-            return@post
-        }
-        controller.withdraw(auth.id)
-    }
-}
 
 /**
  * GraphQL用のビルダ
