8401f5548541b1c72b780127f4a3747451d5d5e7
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-07 18:55:08 +0900

    :sparkles: FindUserByIdQueryUseCaseInteractor.kt

---
+++ backend/src/main/kotlin/com/uramnoil/serverist/serverist/infrastructure/application/user/queries/FindUserByIdQueryUseCaseInteractor.kt
@@ -0,0 +1,14 @@
+package com.uramnoil.serverist.serverist.infrastructure.application.user.queries
+
+import com.benasher44.uuid.Uuid
+import com.uramnoil.serverist.domain.common.user.Id
+import com.uramnoil.serverist.domain.user.repositories.UserRepository
+import com.uramnoil.serverist.serverist.application.user.User
+import com.uramnoil.serverist.serverist.application.user.queries.FindUserByIdQueryUseCaseInputPort
+import com.uramnoil.serverist.serverist.user.infrastructure.application.toApplicationUser
+
+class FindUserByIdQueryUseCaseInteractor(private val repository: UserRepository) : FindUserByIdQueryUseCaseInputPort {
+    override suspend fun execute(id: Uuid): Result<User?> {
+        return repository.findById(Id(id)).map { it?.toApplicationUser() }
+    }
+}
