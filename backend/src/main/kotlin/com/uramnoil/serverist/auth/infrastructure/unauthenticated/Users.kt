f5cc960b6a299de5b6cfc68d021588093e56e9b4
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-10 00:38:37 +0900

    :bug: `account_id`カラムを削除

--- backend/src/main/kotlin/com/uramnoil/serverist/auth/infrastructure/unauthenticated/Users.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/auth/infrastructure/unauthenticated/Users.kt
@@ -3,7 +3,6 @@ package com.uramnoil.serverist.auth.infrastructure.unauthenticated
 import org.jetbrains.exposed.dao.id.UUIDTable
 
 object Users : UUIDTable("unauthenticated_users") {
-    val accountId = char("account_id", 16).uniqueIndex()
     val email = char("email", 255).uniqueIndex()
     val hashedPassword = char("hashed_password", 255)
     val activateCode = uuid("activate_code")
