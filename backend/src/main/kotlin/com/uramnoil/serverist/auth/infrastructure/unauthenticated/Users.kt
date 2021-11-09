8c3ca424c265a7dd81a5ee414c961459e16ebaf7
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-10 00:36:59 +0900

    :bug: テーブル名を修正

--- backend/src/main/kotlin/com/uramnoil/serverist/auth/infrastructure/unauthenticated/Users.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/auth/infrastructure/unauthenticated/Users.kt
@@ -2,7 +2,7 @@ package com.uramnoil.serverist.auth.infrastructure.unauthenticated
 
 import org.jetbrains.exposed.dao.id.UUIDTable
 
-object Users : UUIDTable("users") {
+object Users : UUIDTable("unauthenticated_users") {
     val accountId = char("account_id", 16).uniqueIndex()
     val email = char("email", 255).uniqueIndex()
     val hashedPassword = char("hashed_password", 255)
