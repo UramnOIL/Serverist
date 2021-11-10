c77e01224b00c3541c131af0566cf2e914ee4a39
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-10 21:17:29 +0900

    :bug: Clock.System.now()に修正

--- domain/serverist/src/commonMain/kotlin/com/uramnoil/serverist/domain/serverist/models/server/CreatedAt.kt
+++ domain/serverist/src/commonMain/kotlin/com/uramnoil/serverist/domain/serverist/models/server/CreatedAt.kt
@@ -1,10 +1,10 @@
 package com.uramnoil.serverist.domain.serverist.models.server
 
+import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
-import kotlinx.datetime.isDistantFuture
 
 data class CreatedAt(val value: Instant) {
     init {
-        if (value.isDistantFuture) throw IllegalArgumentException("Only the present or past can be substituted.")
+        if (value > Clock.System.now()) throw IllegalArgumentException("Only the present or past can be substituted.")
     }
 }
