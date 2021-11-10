14b1850842645248b3331a6927f57e5b7e4c7f08
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-10 21:12:12 +0900

    :art: 現在または過去かどうかチェックする

--- domain/serverist/src/commonMain/kotlin/com/uramnoil/serverist/domain/serverist/models/server/CreatedAt.kt
+++ domain/serverist/src/commonMain/kotlin/com/uramnoil/serverist/domain/serverist/models/server/CreatedAt.kt
@@ -1,5 +1,10 @@
 package com.uramnoil.serverist.domain.serverist.models.server
 
 import kotlinx.datetime.Instant
+import kotlinx.datetime.isDistantFuture
 
-data class CreatedAt(val value: Instant)
+data class CreatedAt(val value: Instant) {
+    init {
+        if (value.isDistantFuture) throw IllegalArgumentException("Only the present or past can be substituted.")
+    }
+}
