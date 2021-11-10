da6547084080d0c8052dacdb347465203972448f
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-10 19:08:03 +0900

    :wrench: `id("io.kotest.multiplatform")`

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,14 @@ pluginManagement {
         val kotlinVersion: String by settings
         val serializationVersion: String by settings
         val dokkaVersion: String by settings
+        val kotestVersion: String by settings
 
         kotlin("jvm") version kotlinVersion
         kotlin("multiplatform") version kotlinVersion
         kotlin("plugin.serialization") version "1.5.31"
         id("org.jetbrains.dokka") version dokkaVersion
         id("com.github.johnrengelman.shadow") version "5.2.0"
+        id("io.kotest.multiplatform") version "5.0.0.5"
     }
 }
 
