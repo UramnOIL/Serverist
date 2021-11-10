7bce78e1722795d1c7215f841e95634302a45236
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-10 21:46:18 +0900

    :art: 文字列の長さをチェックする

--- domain/serverist/src/commonMain/kotlin/com/uramnoil/serverist/domain/serverist/models/server/Name.kt
+++ domain/serverist/src/commonMain/kotlin/com/uramnoil/serverist/domain/serverist/models/server/Name.kt
@@ -2,8 +2,11 @@ package com.uramnoil.serverist.domain.serverist.models.server
 
 data class Name(val value: String) {
     init {
-        if (value == "") {
-            throw IllegalArgumentException("空文字は代入できません。")
+        if (value.isBlank()) {
+            throw IllegalArgumentException("Empty characters cannot be assigned.")
+        }
+        if (value.length > 31) {
+            throw IllegalArgumentException("Only up to 31 characters can be substituted.")
         }
     }
 }
