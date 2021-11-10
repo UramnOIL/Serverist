a4da4c810f2b51eadc1ae130334a97b64ceb7680
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-10 18:07:37 +0900

    :bug: 255文字を許容する

--- domain/serverist/src/commonMain/kotlin/com/uramnoil/serverist/domain/serverist/models/user/Description.kt
+++ domain/serverist/src/commonMain/kotlin/com/uramnoil/serverist/domain/serverist/models/user/Description.kt
@@ -2,7 +2,7 @@ package com.uramnoil.serverist.domain.serverist.models.user
 
 data class Description(val value: String) {
     init {
-        if (value.length > 255) {
+        if (value.length >= 256) {
             throw IllegalArgumentException("255文字以下にしてください。")
         }
     }
