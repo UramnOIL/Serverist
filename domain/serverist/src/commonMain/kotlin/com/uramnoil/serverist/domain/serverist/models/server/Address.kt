586f242d0095a3a10e650b30f3d12b84857bf603
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-10 18:51:12 +0900

    :art: チェックの追加

--- domain/serverist/src/commonMain/kotlin/com/uramnoil/serverist/domain/serverist/models/server/Address.kt
+++ domain/serverist/src/commonMain/kotlin/com/uramnoil/serverist/domain/serverist/models/server/Address.kt
@@ -3,7 +3,17 @@ package com.uramnoil.serverist.domain.serverist.models.server
 data class Address(val value: String?) {
     init {
         if (value == "") {
-            throw IllegalArgumentException("空文字は代入できません。")
+            throw IllegalArgumentException("Empty characters cannot be assigned.")
+        }
+        val ipRegex =
+            Regex(pattern = """^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$""")
+        val hostRegex =
+            Regex(pattern = """^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])${'$'}""")
+
+        if (value != null) {
+            if (!(ipRegex.matches(value) || hostRegex.matches(value))) {
+                throw IllegalArgumentException("Incorrect IP or host format.")
+            }
         }
     }
 }
