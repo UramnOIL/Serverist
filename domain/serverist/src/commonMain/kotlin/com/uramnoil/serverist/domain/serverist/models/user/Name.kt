63a22397f22d2d1a15569ce289bcb9adef31061e
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-11 11:23:50 +0900

    :art: 31文字に変更

--- domain/serverist/src/commonMain/kotlin/com/uramnoil/serverist/domain/serverist/models/user/Name.kt
+++ domain/serverist/src/commonMain/kotlin/com/uramnoil/serverist/domain/serverist/models/user/Name.kt
@@ -2,8 +2,8 @@ package com.uramnoil.serverist.domain.serverist.models.user
 
 data class Name(val value: String) {
     init {
-        if (value.length > 63) {
-            throw IllegalArgumentException("63文字以下にしてください。")
+        if (value.length > 31) {
+            throw IllegalArgumentException("Strings longer than 31 characters are not allowed.")
         }
     }
 }
