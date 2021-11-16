bc7a478026e369d9f8f594734bf4f2e64cba6ba7
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-16 14:58:23 +0900

    :bulb: Password

--- domain/auth/src/commonMain/kotlin/com/uramnoil/serverist/domain/auth/kernel/model/Password.kt
+++ domain/auth/src/commonMain/kotlin/com/uramnoil/serverist/domain/auth/kernel/model/Password.kt
@@ -1,5 +1,10 @@
 package com.uramnoil.serverist.domain.auth.kernel.model
 
+/**
+ * Password Value Object
+ * 8文字以上
+ * 半角英数字 + 記号
+ */
 data class Password(val value: String) {
     init {
         val regex = Regex(pattern = """^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9!"#$%&'()*+,\-./:;<=>?\[\\\]^_`{|}~]{8,}$""")
