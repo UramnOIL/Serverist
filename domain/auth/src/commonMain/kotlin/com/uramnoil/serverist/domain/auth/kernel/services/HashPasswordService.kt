3252779a8e3626c78e39dbb8d6d95f75df7dad46
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-16 15:23:31 +0900

    :bulb: HashPasswordService

--- domain/auth/src/commonMain/kotlin/com/uramnoil/serverist/domain/auth/kernel/services/HashPasswordService.kt
+++ domain/auth/src/commonMain/kotlin/com/uramnoil/serverist/domain/auth/kernel/services/HashPasswordService.kt
@@ -3,8 +3,20 @@ package com.uramnoil.serverist.domain.auth.kernel.services
 import com.uramnoil.serverist.domain.auth.kernel.model.HashedPassword
 import com.uramnoil.serverist.domain.auth.kernel.model.Password
 
+/**
+ * パスワードのハッシュと評価
+ */
 interface HashPasswordService {
+    /**
+     * 生パスワードのハッシュ
+     * @param password  生パスワード
+     * @return          ハッシュ済みパスワード
+     */
     fun hash(password: Password): HashedPassword
+
+    /**
+     * パスワードとハッシュ済みパスワードの評価
+     */
     fun check(
         password: Password, hashedPassword: HashedPassword
     ): Boolean
