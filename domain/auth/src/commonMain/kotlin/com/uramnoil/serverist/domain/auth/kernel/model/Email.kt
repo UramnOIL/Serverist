5fe35534a991cd83cc9027d08239f1434ec50ee8
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-16 14:31:52 +0900

    :bulb: Email

--- domain/auth/src/commonMain/kotlin/com/uramnoil/serverist/domain/auth/kernel/model/Email.kt
+++ domain/auth/src/commonMain/kotlin/com/uramnoil/serverist/domain/auth/kernel/model/Email.kt
@@ -1,5 +1,12 @@
 package com.uramnoil.serverist.domain.auth.kernel.model
 
+/**
+ * Email Value Object
+ * RFC5322にほぼ準拠
+ *
+ * @param   value                       Emailの文字列
+ * @throws  IllegalArgumentException    不正な値を渡された場合
+ */
 data class Email(val value: String) {
     init {
         if (value.length > 255) {
