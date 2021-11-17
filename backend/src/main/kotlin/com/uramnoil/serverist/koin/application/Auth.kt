7222f85bd70f0d1c6c5df03c186be379d78d643e
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-17 12:03:13 +0900

    :art: ファクトリに変更

--- backend/src/main/kotlin/com/uramnoil/serverist/koin/application/Auth.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/koin/application/Auth.kt
@@ -1,65 +1,78 @@
 package com.uramnoil.serverist.koin.application
 
-import com.uramnoil.serverist.auth.application.unauthenticated.service.SendEmailToAuthenticateUseCase
+import com.uramnoil.serverist.application.unauthenticated.commands.DeleteUserCommandUseCaseOutputPort
 import com.uramnoil.serverist.auth.infrastructure.HashPasswordServiceImpl
 import com.uramnoil.serverist.auth.infrastructure.authenticated.application.queries.ExposedFindUserByEmailAndPasswordQueryInteractor
-import com.uramnoil.serverist.auth.infrastructure.authenticated.domain.ExposedUserRepositoryImpl
 import com.uramnoil.serverist.auth.infrastructure.unauthenticated.application.commands.CreateUserCommandUseCaseInteractor
 import com.uramnoil.serverist.auth.infrastructure.unauthenticated.application.queries.FindUserByActivationCodeQueryUseCaseInteractor
 import com.uramnoil.serverist.auth.infrastructure.unauthenticated.application.queries.FindUserByEmailQueryUseCaseInteractor
-import com.uramnoil.serverist.auth.infrastructure.unauthenticated.application.queries.FindUserByIdQueryUseCaseInteractor
-import com.uramnoil.serverist.auth.infrastructure.unauthenticated.application.services.SpringBootSendEmailToAuthenticateService
-import com.uramnoil.serverist.auth.infrastructure.unauthenticated.domain.repositories.ExposedUserRepository
-import com.uramnoil.serverist.domain.auth.unauthenticated.repositories.UserRepository
+import com.uramnoil.serverist.auth.infrastructure.unauthenticated.application.services.SpringBootSendEmailToAuthenticateServiceInputPort
 import com.uramnoil.serverist.presenter.AuthController
 import com.uramnoil.serverist.presenter.UserController
 import io.ktor.application.*
+import kotlin.coroutines.CoroutineContext
 import com.uramnoil.serverist.auth.infrastructure.authenticated.application.commands.CreateUserCommandUseCaseInteractor as CreateAuthenticatedUserCommandUseCaseInteractor
 import com.uramnoil.serverist.auth.infrastructure.authenticated.application.commands.DeleteUserCommandUseCaseInteractor as DeleteAuthenticatedUserCommandUseCaseInteractor
-import com.uramnoil.serverist.auth.infrastructure.unauthenticated.application.commands.DeleteUserCommandUseCaseInteractor as DeleteUnauthenticatedUserCommandUseCaseInteractor1
+import com.uramnoil.serverist.auth.infrastructure.unauthenticated.application.commands.DeleteUserCommandUseCaseInteractor as DeleteUnauthenticatedUserCommandUseCaseInteractor
+import com.uramnoil.serverist.domain.auth.authenticated.repositories.UserRepository as AuthenticatedUserRepository
+import com.uramnoil.serverist.domain.auth.unauthenticated.repositories.UserRepository as UnauthenticatedUserRepository
 
-fun Application.buildAuthController(userController: UserController): AuthController {
+fun Application.buildAuthController(
+    unauthenticatedRepository: UnauthenticatedUserRepository,
+    authenticatedUserRepository: AuthenticatedUserRepository,
+    userController: UserController
+): AuthController {
     val hashPasswordService = HashPasswordServiceImpl()
-
-    val unauthenticatedUserRepository: UserRepository = ExposedUserRepository()
-
-    val createUnauthenticatedUserCommandInputPort =
-        CreateUserCommandUseCaseInteractor(unauthenticatedUserRepository, hashPasswordService)
-    val deleteUnauthenticatedUserCommandInputPort =
-        DeleteUnauthenticatedUserCommandUseCaseInteractor1(unauthenticatedUserRepository)
-    val findUserByIdQueryUseCaseInputPort = FindUserByIdQueryUseCaseInteractor(unauthenticatedUserRepository)
-    val findUserByActivationCodeQueryUseCaseInputPort = FindUserByActivationCodeQueryUseCaseInteractor()
-    val findUserByEmailQueryUseCaseInputPort = FindUserByEmailQueryUseCaseInteractor()
-    val findUserByEmailAndPasswordQueryUseCaseInputPort =
-        ExposedFindUserByEmailAndPasswordQueryInteractor(hashPasswordService)
-
-    val sendEmailToAuthenticateUseCase: SendEmailToAuthenticateUseCase = environment.config.run {
-        SpringBootSendEmailToAuthenticateService(
-            property("mail.host").getString(),
-            property("mail.port").getString().toInt(),
-            property("mail.user").getString(),
-            property("mail.password").getString(),
-            property("mail.from").getString(),
-            property("mail.activate_url").getString()
-        )
-    }
-
-    val authenticatedRepository = ExposedUserRepositoryImpl()
-
-    val createAuthenticatedUserCommandInputPort =
-        CreateAuthenticatedUserCommandUseCaseInteractor(authenticatedRepository, hashPasswordService)
-    val deleteAuthenticatedUserCommandInputPort =
-        DeleteAuthenticatedUserCommandUseCaseInteractor(authenticatedRepository)
-
     return AuthController(
-        createUnauthenticatedUserCommandInputPort,
-        deleteUnauthenticatedUserCommandInputPort,
-        sendEmailToAuthenticateUseCase = sendEmailToAuthenticateUseCase,
-        findUserByEmailQueryUseCaseInputPort = findUserByEmailQueryUseCaseInputPort,
-        findUserByActivationCodeQueryUseCaseInputPort = findUserByActivationCodeQueryUseCaseInputPort,
-        findUserByEmailAndPasswordQueryUseCaseInputPort = findUserByEmailAndPasswordQueryUseCaseInputPort,
-        createAuthenticatedCommandUserCaseInputPort = createAuthenticatedUserCommandInputPort,
-        deleteAuthenticatedUserCommandUseCaseInputPort = deleteAuthenticatedUserCommandInputPort,
+        createUnauthenticatedUserCommandUseCaseInputPortFactory = { coroutineContext, outputPort ->
+            CreateUserCommandUseCaseInteractor(
+                unauthenticatedRepository,
+                hashPasswordService,
+                outputPort,
+                coroutineContext
+            )
+        },
+        deleteUnauthenticatedUserCommandUseCaseInputPortFactory = { coroutineContext: CoroutineContext, outputPort: DeleteUserCommandUseCaseOutputPort ->
+            DeleteUnauthenticatedUserCommandUseCaseInteractor(
+                unauthenticatedRepository,
+                outputPort,
+                coroutineContext
+            )
+        },
+        sendEmailToAuthenticateUseCaseInputPortFactory = { coroutineContext, outputPort ->
+            environment.config.run {
+                SpringBootSendEmailToAuthenticateServiceInputPort(
+                    property("mail.host").getString(),
+                    property("mail.port").getString().toInt(),
+                    property("mail.user").getString(),
+                    property("mail.password").getString(),
+                    property("mail.from").getString(),
+                    property("mail.activate_url").getString(),
+                    outputPort,
+                    coroutineContext
+                )
+            }
+        },
+        findUserByEmailQueryUseCaseInputPortFactory = { coroutineContext, outputPort ->
+            FindUserByEmailQueryUseCaseInteractor(outputPort, coroutineContext)
+        },
+        findUserByActivationCodeQueryUseCaseInputPortFactory = { coroutineContext, outputPort ->
+            FindUserByActivationCodeQueryUseCaseInteractor(outputPort, coroutineContext)
+        },
+        findUserByEmailAndPasswordQueryUseCaseInputPortFactory = { coroutineContext, outputPort ->
+            ExposedFindUserByEmailAndPasswordQueryInteractor(hashPasswordService, outputPort, coroutineContext)
+        },
+        createAuthenticatedCommandUserCaseInputPortFactory = { coroutineContext, outputPort ->
+            CreateAuthenticatedUserCommandUseCaseInteractor(
+                authenticatedUserRepository,
+                hashPasswordService,
+                outputPort,
+                coroutineContext
+            )
+        },
+        deleteAuthenticatedUserCommandUseCaseInputPortFactory = { coroutineContext, outputPort ->
+            DeleteAuthenticatedUserCommandUseCaseInteractor(authenticatedUserRepository, outputPort, coroutineContext)
+        },
         userController = userController
     )
 }
--- backend/src/main/kotlin/com/uramnoil/serverist/koin/application/Serverist.kt
+++ backend/src/main/kotlin/com/uramnoil/serverist/koin/application/Serverist.kt
@@ -1,5 +1,7 @@
 package com.uramnoil.serverist.koin.application
 
+import com.uramnoil.serverist.domain.serverist.repositories.ServerRepository
+import com.uramnoil.serverist.domain.serverist.repositories.UserRepository
 import com.uramnoil.serverist.presenter.ServerController
 import com.uramnoil.serverist.presenter.UserController
 import com.uramnoil.serverist.serverist.infrastructure.application.server.commands.CreateServerCommandInteractor
@@ -12,50 +14,50 @@ import com.uramnoil.serverist.serverist.infrastructure.application.server.servic
 import com.uramnoil.serverist.serverist.infrastructure.application.user.command.CreateUserCommandUseCaseInteractor
 import com.uramnoil.serverist.serverist.infrastructure.application.user.command.DeleteUserCommandUseCaseInteractor
 import com.uramnoil.serverist.serverist.infrastructure.application.user.command.UpdateUserCommandUseCaseInteractor
-import com.uramnoil.serverist.serverist.infrastructure.application.user.queries.ExposedFindAllUsersQueryUseCaseInteractor
-import com.uramnoil.serverist.serverist.infrastructure.application.user.queries.ExposedFindUserByAccountIdQueryUseCaseInteractor
-import com.uramnoil.serverist.serverist.infrastructure.application.user.queries.ExposedFindUserByNameQueryUseCaseInteractor
 import com.uramnoil.serverist.serverist.infrastructure.application.user.queries.FindUserByIdQueryUseCaseInteractor
-import com.uramnoil.serverist.serverist.infrastructure.repositories.ExposedServerRepository
-import com.uramnoil.serverist.serverist.infrastructure.repositories.ExposedUserRepository
 import io.ktor.application.*
 
 data class Controllers(val userController: UserController, val serverController: ServerController)
 
-fun Application.buildServeristControllers(): Controllers {
-    val userRepository = ExposedUserRepository()
-    val serverRepository = ExposedServerRepository()
-    val createServerCommand = CreateServerCommandInteractor(userRepository, serverRepository)
-    val deleteServerCommand = DeleteServerCommandUseCaseInteractor(serverRepository)
-    val updateServerCommandUseCaseInputPort = UpdateServerCommandUseCaseInteractor(serverRepository)
-    val findServerByIdQueryUseCaseInputPort = FindServerByIdQueryUseCaseInteractor()
-    val findAllServersQueryUseCaseInputPort = ExposedFindAllServersQueryUseCaseInteractor()
-    val findServersByOwnerQueryUseCaseInputPort = ExposedFindServersByOwnerQueryInteractor()
-    val serverService = ExposedServerService()
-
+fun Application.buildServeristControllers(
+    userRepository: UserRepository,
+    serverRepository: ServerRepository
+): Controllers {
     val serverController = ServerController(
-        service = serverService,
-        createCommand = createServerCommand,
-        updateCommand = updateServerCommandUseCaseInputPort,
-        deleteCommand = deleteServerCommand,
-        findAllQuery = findAllServersQueryUseCaseInputPort,
-        findByIdQuery = findServerByIdQueryUseCaseInputPort,
-        findByOwnerQuery = findServersByOwnerQueryUseCaseInputPort
+        service = ExposedServerService(),
+        createServerCommandUserCaseInputPortFactory = { coroutineContext, outputPort ->
+            CreateServerCommandInteractor(userRepository, serverRepository, outputPort, coroutineContext)
+        },
+        updateServerCommandUserCaseInputPortFactory = { coroutineContext, outputPort ->
+            UpdateServerCommandUseCaseInteractor(serverRepository, outputPort, coroutineContext)
+        },
+        deleteServerCommandUserCaseInputPortFactory = { coroutineContext, outputPort ->
+            DeleteServerCommandUseCaseInteractor(serverRepository, outputPort, coroutineContext)
+        },
+        findByOwnerServerQueryUserCaseInputPortFactory = { coroutineContext, outputPort ->
+            ExposedFindServersByOwnerQueryInteractor(outputPort, coroutineContext)
+        },
+        findByIdServerQueryUserCaseInputPortFactory = { coroutineContext, outputPort ->
+            FindServerByIdQueryUseCaseInteractor(outputPort, coroutineContext)
+        },
+        findAllServerQueryUserCaseInputPortFactory = { coroutineContext, outputPort ->
+            ExposedFindAllServersQueryUseCaseInteractor(outputPort, coroutineContext)
+        }
     )
 
-    val createUserCommandUseCaseInputPort = CreateUserCommandUseCaseInteractor(userRepository)
-    val deleteUserCommandUseCaseInputPort = DeleteUserCommandUseCaseInteractor(userRepository)
-    val updateUserCommandUseCaseInputPort = UpdateUserCommandUseCaseInteractor(userRepository)
-    val findUserByIdQueryUseCaseInputPort = FindUserByIdQueryUseCaseInteractor(userRepository)
-    val fndAllUsersQueryUseCaseInputPort = ExposedFindAllUsersQueryUseCaseInteractor()
-    val findUserByAccountIdQueryUseCaseInputPort = ExposedFindUserByAccountIdQueryUseCaseInteractor()
-    val findUserByNameQueryUseCaseInputPort = ExposedFindUserByNameQueryUseCaseInteractor()
-
     val userController = UserController(
-        createUserCommandUseCaseInputPort = createUserCommandUseCaseInputPort,
-        updateUserCommandUseCaseInputPort = updateUserCommandUseCaseInputPort,
-        deleteUserCommandUseCaseInputPort = deleteUserCommandUseCaseInputPort,
-        findUserByIdQueryUseCaseInputPort = findUserByIdQueryUseCaseInputPort
+        createUserCommandUseCaseInputPortFactory = { coroutineContext, outputPort ->
+            CreateUserCommandUseCaseInteractor(userRepository, outputPort, coroutineContext)
+        },
+        updateUserCommandUseCaseInputPortFactory = { coroutineContext, outputPort ->
+            UpdateUserCommandUseCaseInteractor(userRepository, outputPort, coroutineContext)
+        },
+        deleteUserCommandUseCaseInputPortFactory = { coroutineContext, outputPort ->
+            DeleteUserCommandUseCaseInteractor(userRepository, outputPort, coroutineContext)
+        },
+        findUserByIdQueryUseCaseInputPortFactory = { coroutineContext, outputPort ->
+            FindUserByIdQueryUseCaseInteractor(userRepository, outputPort, coroutineContext)
+        },
     )
 
     return Controllers(userController, serverController)
