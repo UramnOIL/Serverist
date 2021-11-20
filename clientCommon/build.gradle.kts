5191dddcae5a43c563e83498b2f10355d076bd88
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-20 16:40:30 +0900

    :heavy_plus_sign: ktor-client-serialization

--- clientCommon/build.gradle.kts
+++ clientCommon/build.gradle.kts
@@ -11,12 +11,14 @@ kotlin {
     val coroutinesVersion: String by project
     val serializationVersion: String by project
     val apolloVersion: String by project
+    val kotestVersion: String by project
 
     sourceSets {
         val commonMain by getting {
             dependencies {
                 implementation(project(":application"))
-                implementation("io.ktor:ktor-client-core:1.6.5")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization:$ktorVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
                 implementation("com.apollographql.apollo:apollo-runtime-kotlin:$apolloVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
