65b2b829ce95bd1de28153fa387587f4d4fa5690
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-22 00:13:16 +0900

    :sparkles: SignupUseCaseInteractor

---
+++ clientCommon/src/commonMain/kotlin/com/uramnoil/serverist/auth/infrastructure/application/authenticated/SignupUseCaseInteractor.kt
@@ -0,0 +1,52 @@
+package com.uramnoil.serverist.auth.infrastructure.application.authenticated
+
+import com.uramnoil.serverist.application.authenticated.SignupUseCaseInputPort
+import com.uramnoil.serverist.application.authenticated.SignupUseCaseOutputPort
+import com.uramnoil.serverist.exceptions.BadRequestException
+import com.uramnoil.serverist.exceptions.InternalServerErrorException
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import kotlin.coroutines.CoroutineContext
+
+
+class SignupUseCaseInteractor(
+    private val host: String,
+    private val httpClient: HttpClient,
+    private val outputPort: SignupUseCaseOutputPort,
+    private val coroutineContext: CoroutineContext
+) : SignupUseCaseInputPort {
+    @Serializable
+    data class Credential(val email: String, val password: String)
+
+    override fun execute(email: String, password: String) {
+        CoroutineScope(coroutineContext).launch {
+            val result = kotlin.runCatching {
+                val response = httpClient.post<HttpResponse>("$host/signup") {
+                    method = HttpMethod.Post
+                    contentType(ContentType.Application.Json)
+                    body = Credential(email, password)
+                }
+
+                val content = response.receive<String>()
+
+                when (response.status) {
+                    HttpStatusCode.OK -> {}
+                    HttpStatusCode.BadRequest -> {
+                        throw BadRequestException(content)
+                    }
+                    else -> {
+                        throw InternalServerErrorException(content)
+                    }
+                }
+            }
+
+            outputPort.handle(result)
+        }
+    }
+}
