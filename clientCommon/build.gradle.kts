c2c7f0d215e3437be261ef1f0b5f32e93acfe473
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-21 23:10:18 +0900

    :heavy_plus_sign: ktor-client-json

--- clientCommon/build.gradle.kts
+++ clientCommon/build.gradle.kts
@@ -18,10 +18,37 @@ kotlin {
             dependencies {
                 implementation(project(":application"))
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-json:$ktorVersion")
                 implementation("io.ktor:ktor-client-serialization:$ktorVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
                 implementation("com.apollographql.apollo:apollo-runtime-kotlin:$apolloVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                // mockk
+                implementation("io.mockk:mockk:1.12.0")
+
+                // kotest
+                implementation("io.kotest:kotest-property:$kotestVersion")
+                implementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+
+                // ktor
+                implementation("io.ktor:ktor-client-mock:$ktorVersion")
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                // mockk
+                implementation("io.mockk:mockk:1.12.0")
+
+                // kotest
+                implementation("io.kotest:kotest-property:$kotestVersion")
+                implementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+
+                // ktor
+                implementation("io.ktor:ktor-client-mock:$ktorVersion")
             }
         }
     }
