46fdf80a13db167a2f1562512ebea1e6da90f0b2
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-21 23:30:53 +0900

    :art: HttpClientをテストのスコープ内に移動

--- clientCommon/src/jvmTest/kotlin/com/uramnoil/serverist/auth/infrastructure/application/authenticated/LoginUseCaseInteractorTest.kt
+++ clientCommon/src/jvmTest/kotlin/com/uramnoil/serverist/auth/infrastructure/application/authenticated/LoginUseCaseInteractorTest.kt
@@ -14,43 +14,43 @@ import kotlinx.serialization.encodeToString
 import java.util.*
 
 class LoginUseCaseInteractorTest : FunSpec({
-    val email = "uramnoil@example.com"
-    val password = "hoge1234"
-    val uuid = UUID.randomUUID()
+    test("正しいログイン情報") {
+        val email = "uramnoil@example.com"
+        val password = "hoge1234"
+        val uuid = UUID.randomUUID()
 
-    val client = HttpClient(MockEngine) {
-        install(JsonFeature) {
-            serializer = KotlinxSerializer()
-            acceptContentTypes = acceptContentTypes + ContentType.Application.Json
-        }
-        engine {
-            addHandler { request ->
-                if ("${request.url.protocol.name}://${request.url.host}${request.url.fullPath}" != "https://serverist.com/login") {
-                    return@addHandler respondError(HttpStatusCode.NotFound)
-                }
-                if (request.method != HttpMethod.Post) {
-                    return@addHandler respondError(HttpStatusCode.BadRequest)
-                }
-                if (request.body.contentType != ContentType.Application.Json) {
-                    return@addHandler respondError(HttpStatusCode.BadRequest)
-                }
+        val client = HttpClient(MockEngine) {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer()
+                acceptContentTypes = acceptContentTypes + ContentType.Application.Json
+            }
+            engine {
+                addHandler { request ->
+                    if ("${request.url.protocol.name}://${request.url.host}${request.url.fullPath}" != "https://serverist.com/login") {
+                        return@addHandler respondError(HttpStatusCode.NotFound)
+                    }
+                    if (request.method != HttpMethod.Post) {
+                        return@addHandler respondError(HttpStatusCode.BadRequest)
+                    }
+                    if (request.body.contentType != ContentType.Application.Json) {
+                        return@addHandler respondError(HttpStatusCode.BadRequest)
+                    }
 
-                @Serializable
-                data class Credential(val email: String, val password: String)
+                    @Serializable
+                    data class Credential(val email: String, val password: String)
 
-                val (e, p) = kotlinx.serialization.json.Json.decodeFromString<Credential>(
-                    request.body.toByteArray().decodeToString()
-                )
-                if (email == e && password == p) {
+                    val (e, p) = kotlinx.serialization.json.Json.decodeFromString<Credential>(
+                        request.body.toByteArray().decodeToString()
+                    )
+
+                    e shouldBe email
+                    p shouldBe password
+
                     respondOk(kotlinx.serialization.json.Json.encodeToString(mapOf("id" to uuid.toString())))
-                } else {
-                    respondError(HttpStatusCode.BadRequest)
                 }
             }
         }
-    }
 
-    test("正しいログイン情報") {
         val useCase = LoginUseCaseInteractor(
             "https://serverist.com",
             client,
