a4fc25ef3830cead34d75cf801341675c4851952
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-11-28 00:25:37 +0900

    :heavy_plus_sign: serialization

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -8,7 +8,10 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
+                val serializationVersion: String by project
+
                 implementation(project(":domain:common"))
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
             }
         }
     }
