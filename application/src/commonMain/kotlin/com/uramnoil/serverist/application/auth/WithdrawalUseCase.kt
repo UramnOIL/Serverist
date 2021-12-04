ff744ec03f92ce8ea08308f664d704cddd90421b
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-12-05 01:55:03 +0900

    :sparkles: WithdrawalUseCaseInputPortForServer

--- application/src/commonMain/kotlin/com/uramnoil/serverist/application/auth/WithdrawalUseCase.kt
+++ application/src/commonMain/kotlin/com/uramnoil/serverist/application/auth/WithdrawalUseCase.kt
@@ -1,7 +1,13 @@
 package com.uramnoil.serverist.application.auth
 
-fun interface WithdrawalUseCaseInputPort {
+import com.benasher44.uuid.Uuid
+
+fun interface WithdrawalUseCaseInputPortForClient {
     fun execute()
+}
+
+fun interface WithdrawalUseCaseInputPortForServer {
+    fun execute(id: Uuid)
 }
 
 fun interface WithdrawalUseCaseOutputPort {
