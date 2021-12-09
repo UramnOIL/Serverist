219a62da03c42d4628e13119bfc5def73bf220a3
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-12-09 23:31:16 +0900

    :truck: 名前変更

--- backend/src/main/kotlin/com/uramnoil/serverist/auth/application/SendActivationEmailService.kt
+++
@@ -1,7 +0,0 @@
-package com.uramnoil.serverist.auth.application
-
-import java.util.*
-
-interface SendActivationEmailService {
-    suspend fun sendEmail(mailAddress: String, activationCode: UUID): Result<Unit>
-}
