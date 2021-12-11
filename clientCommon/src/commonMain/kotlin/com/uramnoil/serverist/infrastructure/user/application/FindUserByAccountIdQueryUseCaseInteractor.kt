23bd64017be67eb4f784b204b832f5e11de44e80
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-12-11 14:17:14 +0900

    :poop: 後回し

--- clientCommon/src/commonMain/kotlin/com/uramnoil/serverist/infrastructure/user/application/FindUserByAccountIdQueryUseCaseInteractor.kt
+++ clientCommon/src/commonMain/kotlin/com/uramnoil/serverist/infrastructure/user/application/FindUserByAccountIdQueryUseCaseInteractor.kt
@@ -1,40 +1,18 @@
 package com.uramnoil.serverist.infrastructure.user.application
 
+import com.apollographql.apollo.ApolloClient
 import com.uramnoil.serverist.application.user.FindUserByAccountIdQueryUseCaseInput
 import com.uramnoil.serverist.application.user.FindUserByAccountIdQueryUseCaseInputPort
 import com.uramnoil.serverist.application.user.FindUserByAccountIdQueryUseCaseOutputPort
-import com.uramnoil.serverist.exceptions.BadRequestException
-import io.ktor.client.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 class FindUserByAccountIdQueryUseCaseInteractor(
-    private val httpClient: HttpClient,
-    private val url: String,
+    private val apolloClient: ApolloClient,
     private val outputPort: FindUserByAccountIdQueryUseCaseOutputPort,
     coroutineContext: CoroutineContext
 ) : FindUserByAccountIdQueryUseCaseInputPort, CoroutineScope by CoroutineScope(coroutineContext) {
     override fun execute(input: FindUserByAccountIdQueryUseCaseInput) {
-        val (id) = input
-        launch {
-            val result = kotlin.runCatching {
-                val response = httpClient.post<HttpResponse>(url) {
-                    body = """
-                        query FindAllUsers {
-                          findAllUsers() {
-                            id
-                          }
-                        }
-                    """.trimIndent()
-                }
-                if (response.status.value == 200) {
-                    throw BadRequestException()
-                }
-            }
-            //outputPort.handle(result)
-        }
+        error("not implemented yet.")
     }
 }
