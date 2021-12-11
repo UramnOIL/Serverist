43fe113babf097eb4b82729f01f2ac88af4dd4cb
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-12-11 16:13:56 +0900

    :sparkles: DeleteServerCommandUseCaseInteractor.kt

---
+++ clientCommon/src/commonMain/kotlin/com/uramnoil/serverist/infrastructure/server/application/DeleteServerCommandUseCaseInteractor.kt
@@ -0,0 +1,49 @@
+package com.uramnoil.serverist.infrastructure.server.application
+
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.ApolloExperimental
+import com.apollographql.apollo.coroutines.await
+import com.uramnoil.serverist.DeleteServerMutation
+import com.uramnoil.serverist.application.server.DeleteServerCommandUseCaseInput
+import com.uramnoil.serverist.application.server.DeleteServerCommandUseCaseInputPort
+import com.uramnoil.serverist.application.server.DeleteServerCommandUseCaseOutputPort
+import io.github.aakira.napier.Napier
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+class DeleteServerCommandUseCaseInteractor(
+    private val apolloClient: ApolloClient,
+    coroutineContext: CoroutineContext,
+    private val outputPort: DeleteServerCommandUseCaseOutputPort,
+) : DeleteServerCommandUseCaseInputPort, CoroutineScope by CoroutineScope(coroutineContext) {
+    @OptIn(ApolloExperimental::class)
+    override fun execute(input: DeleteServerCommandUseCaseInput) {
+        launch {
+            val mutation = input.run {
+                DeleteServerMutation(
+                    id = input.id.toString()
+                )
+            }
+            val deleteServerResult = apolloClient.mutate(mutation).await()
+
+            deleteServerResult.errors?.run {
+                forEach {
+                    Napier.e(it.message)
+                }
+                outputPort.handle(Result.failure(RuntimeException("Errors returned.")))
+                return@launch
+            }
+
+            val data = deleteServerResult.data
+
+            data ?: run {
+                // Data is null
+                outputPort.handle(Result.failure(IllegalStateException("No data returned.")))
+                return@launch
+            }
+
+            outputPort.handle(Result.success(Unit))
+        }
+    }
+}
