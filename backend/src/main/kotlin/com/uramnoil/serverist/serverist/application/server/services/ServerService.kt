364004f7d29420fa90b07fbc25286817740b944b
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-12-13 20:55:03 +0900

    :wrench: `:application`プロジェクトに依存

--- backend/src/main/kotlin/com/uramnoil/serverist/serverist/application/server/services/ServerService.kt
+++
@@ -1,8 +0,0 @@
-package com.uramnoil.serverist.serverist.application.server.services
-
-import com.benasher44.uuid.Uuid
-
-
-interface ServerService {
-    suspend fun checkUserIsOwnerOfServer(ownerId: Uuid, serverId: Uuid): Result<Boolean>
-}
