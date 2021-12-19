e7361db2370b5888c9dda09b070960f024766d54
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-12-19 14:50:43 +0900

    :bug: InputPortに変更

--- clientCommon/src/clientMain/kotlin/com/uramnoil/serverist/presentation/SettingController.kt
+++ clientCommon/src/clientMain/kotlin/com/uramnoil/serverist/presentation/SettingController.kt
@@ -2,13 +2,10 @@ package com.uramnoil.serverist.presentation
 
 import com.uramnoil.serverist.application.user.UpdateUserUseCaseInput
 import com.uramnoil.serverist.application.user.UpdateUserUseCaseInputPort
-import com.uramnoil.serverist.application.user.UpdateUserUseCaseOutputPort
 
 class SettingController(
-    updateUserUseCaseOutputPort: UpdateUserUseCaseOutputPort
+    private val updateUserUseCaseInputPort: UpdateUserUseCaseInputPort
 ) {
-    private val updateUserUseCaseInputPort: UpdateUserUseCaseInputPort = TODO("DIコンテナの導入")
-
     fun updateUser(accountId: String, name: String, description: String) {
         updateUserUseCaseInputPort.execute(UpdateUserUseCaseInput(accountId, name, description))
     }
