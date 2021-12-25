4b6b119c7296b8a5d4f365150a9a4ac656e786ee
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-12-25 12:36:30 +0900

    :art: Intに変更

--- domain/auth/src/commonMain/kotlin/com/uramnoil/serverist/domain/auth/unauthenticated/models/ActivationCode.kt
+++ domain/auth/src/commonMain/kotlin/com/uramnoil/serverist/domain/auth/unauthenticated/models/ActivationCode.kt
@@ -1,6 +1,4 @@
 package com.uramnoil.serverist.domain.auth.unauthenticated.models
 
-import com.benasher44.uuid.Uuid
 
-
-data class ActivationCode(val value: Uuid)
+data class ActivationCode(val value: Int)
