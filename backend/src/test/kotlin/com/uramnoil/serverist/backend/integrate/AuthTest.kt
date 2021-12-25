adc208414f28cba842eb8e869cdfd98148a2dfd7
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-12-25 11:27:52 +0900

    :white_check_mark: signinに変更

--- backend/src/test/kotlin/com/uramnoil/serverist/backend/integrate/AuthTest.kt
+++ backend/src/test/kotlin/com/uramnoil/serverist/backend/integrate/AuthTest.kt
@@ -16,10 +16,16 @@ import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
-import io.ktor.application.*
-import io.ktor.config.*
-import io.ktor.http.*
-import io.ktor.server.testing.*
+import io.ktor.application.Application
+import io.ktor.config.MapApplicationConfig
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.cookiesSession
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.setBody
+import io.ktor.server.testing.withTestApplication
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.jetbrains.exposed.sql.Database
@@ -27,7 +33,7 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.util.*
+import java.util.UUID
 import com.uramnoil.serverist.serverist.infrastructure.Users as ServeristUsers
 
 class AuthTest : FunSpec({
@@ -165,7 +171,7 @@ class AuthTest : FunSpec({
         }
     }
 
-    test("/login test") {
+    test("/signin test") {
         withTestApplication(
             moduleFunction = {
                 mailConfig()
@@ -184,7 +190,7 @@ class AuthTest : FunSpec({
                 }
             }
             cookiesSession {
-                with(handleRequest(HttpMethod.Post, "/login") {
+                with(handleRequest(HttpMethod.Post, "/signin") {
                     addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
                     setBody(Json.encodeToString(mapOf("email" to email, "password" to password)))
                 }) {
@@ -221,7 +227,7 @@ class AuthTest : FunSpec({
                 }
             }
             cookiesSession {
-                with(handleRequest(HttpMethod.Post, "/login") {
+                with(handleRequest(HttpMethod.Post, "/signin") {
                     addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
                     setBody(Json.encodeToString(mapOf("email" to email, "password" to password)))
                 }) {
