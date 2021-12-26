0e671379c4a88f3feaac33e584dd68ad6187c293
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-12-26 16:13:26 +0900

    :sparkles: AuthPresenter.kt

---
+++ clientCommon/src/clientMain/kotlin/com/uramnoil/serverist/presentation/AuthPresenter.kt
@@ -0,0 +1,15 @@
+package com.uramnoil.serverist.presentation
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class AuthViewModel(
+    private val mutableSignUpErrorStateFlow: MutableStateFlow<Throwable?>,
+    private val mutableSignInErrorStateFlow: MutableStateFlow<Throwable?>,
+) {
+    val signUpErrorStateFlow: StateFlow<Throwable?>
+        get() = mutableSignUpErrorStateFlow
+
+    val signInErrorStateFlow: StateFlow<Throwable?>
+        get() = mutableSignInErrorStateFlow
+}
