2ccbb890341d33b083ad52d981db79d12afc66e9
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-12-28 19:31:10 +0900

    :construction: Main.kt

--- webCompose/src/jsMain/kotlin/Main.kt
+++ webCompose/src/jsMain/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -12,9 +13,12 @@ import com.uramnoil.serverist.SessionId
 import com.uramnoil.serverist.infrastructure.auth.application.ActivateUseCaseInteractor
 import com.uramnoil.serverist.infrastructure.auth.application.SignInUseCaseInteractor
 import com.uramnoil.serverist.infrastructure.auth.application.SignUpUseCaseInteractor
+import com.uramnoil.serverist.infrastructure.server.application.CreateServerUseCaseInteractor
 import com.uramnoil.serverist.infrastructure.server.application.FindAllServersUseCaseInteractor
 import com.uramnoil.serverist.presentation.AuthController
 import com.uramnoil.serverist.presentation.AuthViewModel
+import com.uramnoil.serverist.presentation.CreateServerController
+import com.uramnoil.serverist.presentation.CreateServerViewModel
 import com.uramnoil.serverist.presentation.SearchServersController
 import com.uramnoil.serverist.presentation.ServersViewModel
 import com.uramnoil.serverist.serverist.application.server.Server
@@ -32,7 +36,9 @@ import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import org.jetbrains.compose.web.renderComposable
 import pages.AuthPage
+import pages.CreateServerPage
 import pages.SearchServersPage
+import kotlin.coroutines.CoroutineContext
 
 class AuthenticationInterceptor(private val sessionId: SessionId) : HttpInterceptor {
     override suspend fun intercept(request: HttpRequest, chain: HttpInterceptorChain): HttpResponse {
@@ -74,84 +80,106 @@ fun main() {
     Napier.base(DebugAntilog())
 
     renderComposable(rootElementId = "root") {
-        val coroutineScope = rememberCoroutineScope()
-        var scene by remember { mutableStateOf(Scene.Serverist) }
+        var scene by remember { mutableStateOf(Scene.Auth) }
 
         when (scene) {
             Scene.Auth -> {
-                val mutableSignUpErrorStateFlow = MutableStateFlow<Throwable?>(null)
-                val mutableActivateErrorStateFlow = MutableStateFlow<Throwable?>(null)
-                val mutableSignInErrorStateFlow = MutableStateFlow<Throwable?>(null)
-
-                val signUpUseCaseInputPort = SignUpUseCaseInteractor(
-                    coroutineContext,
-                    sessionId,
-                    host,
-                    httpClient,
-                ) {
-                    it.result.onFailure {
-                        coroutineScope.launch {
-                            Napier.e("Error", it)
-                            mutableSignUpErrorStateFlow.emit(it)
-                        }
-                    }
+                Auth(coroutineContext, httpClient, sessionId) {
+                    scene = it
                 }
-
-                val activateUseCaseInputPort = ActivateUseCaseInteractor(coroutineContext, httpClient, host) {
-                    it.result.onFailure {
-                        coroutineScope.launch {
-                            Napier.e("Error", it)
-                            mutableActivateErrorStateFlow.emit(it)
-                        }
-                    }
-                }
-
-                val signInUseCaseInputPort = SignInUseCaseInteractor(
-                    coroutineContext,
-                    host,
-                    httpClient,
-                    sessionId,
-                ) {
-                    it.result.fold(
-                        {
-                            scene = Scene.Serverist
-                        }, {
-                            coroutineScope.launch {
-                                Napier.e("Error", it)
-                                mutableSignInErrorStateFlow.emit(it)
-                            }
-                        }
-                    )
-                }
-                AuthPage(
-                    AuthController(signUpUseCaseInputPort, activateUseCaseInputPort, signInUseCaseInputPort),
-                    AuthViewModel(
-                        mutableSignUpErrorStateFlow,
-                        mutableSignInErrorStateFlow,
-                        mutableActivateErrorStateFlow
-                    )
-                )
             }
             Scene.Serverist -> {
-                val mutableServersStateFlow = MutableStateFlow<List<Server>>(listOf())
-                val serversViewModel = ServersViewModel(mutableServersStateFlow)
-                val findAllServersUseCaseInputPort = FindAllServersUseCaseInteractor(
-                    coroutineContext,
-                    apolloClient
-                ) {
-                    it.result.fold(
-                        {
-                            coroutineScope.launch {
-                                mutableServersStateFlow.emit(it)
-                            }
-                        },
-                        {
-
-                        }
-                    )
-                }
-                SearchServersPage(SearchServersController(findAllServersUseCaseInputPort), serversViewModel)
+                Serverist(coroutineContext, apolloClient)
             }
         }
     }
+}
+
+@Composable
+fun Auth(coroutineContext: CoroutineContext, httpClient: HttpClient, sessionId: SessionId, setScene: (Scene) -> Unit) {
+    val coroutineScope = rememberCoroutineScope()
+    val mutableSignUpErrorStateFlow = MutableStateFlow<Throwable?>(null)
+    val mutableActivateErrorStateFlow = MutableStateFlow<Throwable?>(null)
+    val mutableSignInErrorStateFlow = MutableStateFlow<Throwable?>(null)
+
+    val signUpUseCaseInputPort = SignUpUseCaseInteractor(
+        coroutineContext,
+        sessionId,
+        host,
+        httpClient,
+    ) {
+        it.result.onFailure {
+            coroutineScope.launch {
+                Napier.e("Error", it)
+                mutableSignUpErrorStateFlow.emit(it)
+            }
+        }
+    }
+
+    val activateUseCaseInputPort = ActivateUseCaseInteractor(coroutineContext, httpClient, host) {
+        it.result.onFailure {
+            coroutineScope.launch {
+                Napier.e("Error", it)
+                mutableActivateErrorStateFlow.emit(it)
+            }
+        }
+    }
+
+    val signInUseCaseInputPort = SignInUseCaseInteractor(
+        coroutineContext,
+        host,
+        httpClient,
+        sessionId,
+    ) {
+        it.result.fold(
+            {
+                setScene(Scene.Serverist)
+            }
+        ) {
+            coroutineScope.launch {
+                Napier.e("Error", it)
+                mutableSignInErrorStateFlow.emit(it)
+            }
+        }
+    }
+    AuthPage(
+        AuthController(signUpUseCaseInputPort, activateUseCaseInputPort, signInUseCaseInputPort),
+        AuthViewModel(
+            mutableSignUpErrorStateFlow,
+            mutableSignInErrorStateFlow,
+            mutableActivateErrorStateFlow
+        )
+    )
+}
+
+@Composable
+fun Serverist(coroutineContext: CoroutineContext, apolloClient: ApolloClient) {
+    val coroutineScope = rememberCoroutineScope()
+
+    val mutableServersStateFlow by remember { mutableStateOf(MutableStateFlow<List<Server>>(listOf())) }
+    val serversViewModel = ServersViewModel(mutableServersStateFlow)
+    val findAllServersUseCaseInputPort = FindAllServersUseCaseInteractor(coroutineContext, apolloClient) {
+        it.result.fold(
+            { servers ->
+                coroutineScope.launch {
+                    mutableServersStateFlow.emit(servers)
+                }
+            },
+            { throwable ->
+                Napier.d("error", throwable)
+            }
+        )
+    }
+
+    SearchServersPage(SearchServersController(findAllServersUseCaseInputPort), serversViewModel)
+
+    val createServerUseCaseInputPort = CreateServerUseCaseInteractor(coroutineContext, apolloClient) {
+        it.result.fold(
+            {
+            },
+            {
+            }
+        )
+    }
+    CreateServerPage(CreateServerController(createServerUseCaseInputPort), CreateServerViewModel())
 }
